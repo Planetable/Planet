f86e405309e6f9cce0576eaa489301276b6374e2
Kai <kai@v2ex.com>
2022-02-26 13:55:40 +0800

    Improved article web view.

--- Planet/Views/PlanetArticleView.swift
+++ Planet/Views/PlanetArticleView.swift
@@ -9,101 +9,20 @@ import SwiftUI
 import WebKit
 
 
-class WebViewModel: ObservableObject {
-    @Published var link: String
-    @Published var didFinishLoading: Bool = false
-    @Published var pageTitle: String
-    
-    init (link: String) {
-        self.link = link
-        self.pageTitle = ""
-    }
-}
-
-
-struct SwiftUIWebView: NSViewRepresentable {
-    
-    public typealias NSViewType = WKWebView
-    @ObservedObject var viewModel: WebViewModel
-
-    private let webView: WKWebView = WKWebView()
-    
-    public func makeNSView(context: NSViewRepresentableContext<SwiftUIWebView>) -> WKWebView {
-        webView.navigationDelegate = context.coordinator
-        webView.uiDelegate = context.coordinator as? WKUIDelegate
-        webView.load(URLRequest(url: URL(string: viewModel.link)!))
-        
-        NotificationCenter.default.addObserver(forName: Notification.Name("SwiftUIWebViewReloadAction"), object: nil, queue: nil) { n in
-            guard let url = n.object as? URL else { return }
-            debugPrint("reloading article: \(url)")
-            DispatchQueue.main.async {
-                self.webView.load(URLRequest(url: URL(string: url.absoluteString)!, cachePolicy: .reloadRevalidatingCacheData, timeoutInterval: 10))
-            }
-        }
-        
-        return webView
-    }
-
-    public func updateNSView(_ nsView: WKWebView, context: NSViewRepresentableContext<SwiftUIWebView>) { }
-
-    public func makeCoordinator() -> Coordinator {
-        return Coordinator(viewModel)
-    }
-    
-    class Coordinator: NSObject, WKNavigationDelegate {
-        private var viewModel: WebViewModel
-
-        init(_ viewModel: WebViewModel) {
-            self.viewModel = viewModel
-        }
-        
-        public func webView(_: WKWebView, didFail: WKNavigation!, withError: Error) { }
-
-        public func webView(_: WKWebView, didFailProvisionalNavigation: WKNavigation!, withError: Error) { }
-
-        public func webView(_ web: WKWebView, didFinish: WKNavigation!) {
-            self.viewModel.pageTitle = web.title!
-//            self.viewModel.link = web.url?.absoluteString
-            self.viewModel.didFinishLoading = true
-        }
-
-        public func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) { }
-
-        public func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
-            decisionHandler(.allow)
-        }
-    }
-}
-
-
-struct SafariWebView: View {
-    @ObservedObject var model: WebViewModel
-
-    init(mesgURL: String) {
-        self.model = WebViewModel(link: mesgURL)
-    }
-    
-    var body: some View {
-        SwiftUIWebView(viewModel: model)
-    }
-}
-
-
 struct PlanetArticleView: View {
     @EnvironmentObject private var planetStore: PlanetStore
 
     var article: PlanetArticle!
+    
+    @State private var url: URL = Bundle.main.url(forResource: "TemplatePlaceholder.html", withExtension: "")!
 
     var body: some View {
         VStack {
             if let article = article, let id = planetStore.selectedArticle, article.id != nil, id == article.id!.uuidString {
-                let placeholderURL = Bundle.main.url(forResource: "TemplatePlaceholder.html", withExtension: "")
-                SafariWebView(mesgURL: placeholderURL!.absoluteString)
+                SimplePlanetArticleView(url: $url)
                     .task(priority: .utility) {
                         if let urlPath = await PlanetManager.shared.articleURL(article: article) {
-                            DispatchQueue.main.async {
-                                NotificationCenter.default.post(name: Notification.Name("SwiftUIWebViewReloadAction"), object: urlPath)
-                            }
+                            url = urlPath
                         } else {
                             DispatchQueue.main.async {
                                 self.planetStore.currentArticle = nil
---
+++ Planet/Views/SimplePlanetArticleView.swift
@@ -0,0 +1,56 @@
+//
+//  SimplePlanetArticleView.swift
+//  Planet
+//
+//  Created by Kai on 2/26/22.
+//
+
+import SwiftUI
+import WebKit
+
+
+struct SimplePlanetArticleView: View {
+    @Binding var url: URL
+    
+    var body: some View {
+        SimpleWebView(url: url)
+    }
+}
+
+struct SimpleWebView: NSViewRepresentable {
+    
+    public typealias NSViewType = WKWebView
+    
+    let url: URL
+    let navigationHelper = WebViewHelper()
+
+    func makeNSView(context: NSViewRepresentableContext<SimpleWebView>) -> WKWebView {
+        let webview = WKWebView()
+        webview.navigationDelegate = navigationHelper
+
+        let request = URLRequest(url: self.url, cachePolicy: .returnCacheDataElseLoad)
+        webview.load(request)
+
+        return webview
+    }
+
+    func updateNSView(_ webview: WKWebView, context: NSViewRepresentableContext<SimpleWebView>) {
+        let request = URLRequest(url: self.url, cachePolicy: .returnCacheDataElseLoad)
+        webview.load(request)
+    }
+}
+
+class WebViewHelper: NSObject, WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+    }
+    
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+    }
+    
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+    }
+    
+    func webView(_ webView: WKWebView, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+        completionHandler(.performDefaultHandling, nil)
+    }
+}
