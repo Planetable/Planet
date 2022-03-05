748f28475ab2a547c72437b218cbd0b8558a6ee9
Livid <livid@v2ex.com>
2022-03-05 11:27:34 -0800

    Fix typo

--- Planet/PlanetCommand.swift
+++ Planet/PlanetCommand.swift
@@ -13,7 +13,7 @@ import Dispatch
  *  Run IPFS commands
  *
  *  - parameter command: The command with launch path, config path and arguments
- *  - parameter process: The process to use to proform the command. default: New one every command
+ *  - parameter process: The process to use to perform the command. default: New one every command
  *  - parameter outputHandle: Any `FileHandle` that any output (STDOUT) should be redirected to
  *              (at the moment this is only supported on macOS)
  *  - parameter errorHandle: Any `FileHandle` that any error output (STDERR) should be redirected to
@@ -45,7 +45,7 @@ struct PlanetCommand {
     let arguments: [String]
     let configPath: String
     let timeout: Int
-    
+
     init(command: String, arguments: [String], configPath: String, timeout: Int = 0) {
         self.command = command
         self.arguments = arguments
@@ -59,12 +59,12 @@ extension PlanetCommand {
         let cmd = PlanetCommand(command: target.path, arguments: ["init"], configPath: config.path)
         return cmd
     }
-    
+
     static func ipfsGetID(target: URL, config: URL) -> PlanetCommand {
         let cmd = PlanetCommand(command: target.path, arguments: ["id"], configPath: config.path)
         return cmd
     }
-    
+
     static func ipfsAddDirectory(target: URL, config: URL, directory: URL) -> PlanetCommand {
         var params: [String] = []
         params.append("add")
@@ -76,27 +76,27 @@ extension PlanetCommand {
         let cmd = PlanetCommand(command: target.path, arguments: params, configPath: config.path)
         return cmd
     }
-    
+
     static func ipfsUpdateAPIPort(target: URL, config: URL, port: String) -> PlanetCommand {
         let cmd = PlanetCommand(command: target.path, arguments: ["config", "Addresses.API", "/ip4/127.0.0.1/tcp/\(port)"], configPath: config.path)
         return cmd
     }
-    
+
     static func ipfsUpdateGatewayPort(target: URL, config: URL, port: String) -> PlanetCommand {
         let cmd = PlanetCommand(command: target.path, arguments: ["config", "Addresses.Gateway", "/ip4/127.0.0.1/tcp/\(port)"], configPath: config.path)
         return cmd
     }
-    
+
     static func ipfsUpdateSwarmPort(target: URL, config: URL, port: String) -> PlanetCommand {
         let cmd = PlanetCommand(command: target.path, arguments: ["config", "Addresses.Swarm", "[\"/ip4/0.0.0.0/tcp/\(port)\", \"/ip6/::/tcp/\(port)\", \"/ip4/0.0.0.0/udp/\(port)/quic\", \"/ip6/::/udp/\(port)/quic\"]", "--json"], configPath: config.path)
         return cmd
     }
-    
+
     static func ipfsLaunchDaemon(target: URL, config: URL) -> PlanetCommand {
         let cmd = PlanetCommand(command: target.path, arguments: ["daemon", "--enable-namesys-pubsub", "--enable-pubsub-experiment"], configPath: config.path)
         return cmd
     }
-    
+
     static func ipfsTerminateDaemon(target: URL, config: URL) -> PlanetCommand {
         let cmd = PlanetCommand(command: target.path, arguments: ["shutdown"], configPath: config.path)
         return cmd
@@ -141,22 +141,22 @@ private extension Process {
     @discardableResult func launchCommand(command: String, withArguments argus: [String], andConfigPath config: String, outputHandle: FileHandle? = nil, errorHandle: FileHandle? = nil) throws -> [String: Any] {
         executableURL = URL(fileURLWithPath: command)
         arguments = argus
-        
+
         var theEnvironment = ProcessInfo.processInfo.environment
         theEnvironment["IPFS_PATH"] = PlanetManager.shared.ipfsENVPath().path
         environment = theEnvironment
 
         let processQueue = DispatchQueue(label: Configuration.bundlePrefix + ".planet.command-output-queue")
-        
+
         var outputData = Data()
         var errorData = Data()
 
         let outputPipe = Pipe()
         standardOutput = outputPipe
-        
+
         let errorPipe = Pipe()
         standardError = errorPipe
-        
+
         outputPipe.fileHandleForReading.readabilityHandler = { handler in
             let data = handler.availableData
             guard data.count > 0 else {
@@ -180,11 +180,11 @@ private extension Process {
                 errorHandle?.write(data)
             }
         }
-        
+
         launch()
-        
+
         waitUntilExit()
-        
+
         if let handle = outputHandle, !handle.isStandard {
             handle.closeFile()
         }
@@ -192,10 +192,10 @@ private extension Process {
         if let handle = errorHandle, !handle.isStandard {
             handle.closeFile()
         }
-        
+
         outputPipe.fileHandleForReading.readabilityHandler = nil
         errorPipe.fileHandleForReading.readabilityHandler = nil
-        
+
         return try processQueue.sync {
             if terminationStatus != 0 {
                 throw PlanetCommandError(
@@ -233,33 +233,33 @@ private extension Data {
 private extension String {
     func processedMultilineCommandOutput() -> String {
         var s = self
-        
+
         if let o = s.removingPercentEncoding {
             s = o
         }
-        
+
         s = s.replacingOccurrences(of: "\t", with: " ")
         s = s.replacingOccurrences(of: "\\", with: "")
-        
+
         return s
     }
-    
+
     func processedCommandOutput() -> [String: Any] {
         var s = self
-        
+
         if let o = s.removingPercentEncoding {
             s = o
         }
-        
+
         s = s.replacingOccurrences(of: "\t", with: " ")
         s = s.replacingOccurrences(of: "\\", with: "")
-        
+
         if let data = s.data(using: .utf8) {
             if let json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any] {
                 return json
             }
         }
-        
+
         let o = s.components(separatedBy: "\n").filter { n in
             if n != "" {
                 return true
@@ -269,7 +269,7 @@ private extension String {
         if o.count > 0 {
             return ["result": o]
         }
-        
+
         return ["result": s]
     }
 }
