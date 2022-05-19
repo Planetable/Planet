3ea50b6de19bbf64679fda4ccb026e03a6a30443
Livid <livid@v2ex.com>
2022-05-19 12:53:55 -0700

    Add "Reveal in Finder" for template

--- Planet/TemplateBrowser/TemplateBrowserSidebar.swift
+++ Planet/TemplateBrowser/TemplateBrowserSidebar.swift
@@ -15,8 +15,20 @@ struct TemplateBrowserSidebar: View {
         List(selection: $selection) {
             ForEach(store.templates, id: \.id) { template in
                 Text(template.name)
+                .contextMenu {
+                    Button(action: {
+                        revealInFinder(template)
+                    }) {
+                        Text("Reveal in Finder")
+                    }
+                }
             }
         }
         .frame(minWidth: 200)
+    }
+
+    private func revealInFinder(_ template: Template) {
+        let url = URL(fileURLWithPath: template.path.path)
+        NSWorkspace.shared.selectFile(nil, inFileViewerRootedAtPath: url.path)
     }
 }
