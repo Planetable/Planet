326347849ed68cd1be518849e0b087916933a8cc
Livid <livid@v2ex.com>
2022-06-04 23:10:11 -0700

    Add versioning.xcconfig during pre-commit

--- git-hooks/pre-commit.sh
+++ git-hooks/pre-commit.sh
@@ -2,3 +2,4 @@
 buildNumber=`git rev-list --count main`
 echo "CURRENT_PROJECT_VERSION = $buildNumber" > Planet/versioning.xcconfig
 echo "Set CURRENT_PROJECT_VERSION to $buildNumber"
+git add Planet/versioning.xcconfig
