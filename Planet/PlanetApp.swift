0d0e42406a94398a020c78d14abe4c0097f27d39
Livid <livid@v2ex.com>
2022-07-10 07:53:15 -0700

    Add 1s to make the migration feel faster?

--- Planet/PlanetApp.swift
+++ Planet/PlanetApp.swift
@@ -170,6 +170,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                 }
             }
             Task { @MainActor in
+                try await Task.sleep(nanoseconds: 1_000_000_000)
                 PlanetStore.shared.isMigrating = false
             }
         }
