1670744dadbdc108b32817e97769883064d55507
Kai <kai@v2ex.com>
2022-08-01 16:21:01 +0800

    WIP: handle downloads by mime type in articles.

--- Planet/Views/ArticleWebView.swift
+++ Planet/Views/ArticleWebView.swift
@@ -5,8 +5,6 @@ import os
 struct ArticleWebView: NSViewRepresentable {
     static let logger = Logger(subsystem: Bundle.main.bundleIdentifier!, category: "ArticleWebView")
 
-    public typealias NSViewType = WKWebView
-
     @Binding var url: URL
 
     func makeCoordinator() -> Coordinator {
@@ -45,9 +43,26 @@ struct ArticleWebView: NSViewRepresentable {
     class Coordinator: NSObject, WKNavigationDelegate {
         let parent: ArticleWebView
 
+        private var navigationType: WKNavigationType = .other
+
         init(_ parent: ArticleWebView) {
             self.parent = parent
         }
+
+        private func shouldHandleDownloadForMimeType(_ mimeType: String) -> Bool {
+            let downloadableMimeTypes: [String] = ["application/pdf", "image/jpeg", "image/png"]
+            return downloadableMimeTypes.contains(mimeType)
+        }
+
+        private func isValidatedLink(_ url: URL) -> Bool {
+            let components = URLComponents(url: url, resolvingAgainstBaseURL: false)
+            if components?.scheme == "http" || components?.scheme == "https" {
+                return true
+            }
+            return false
+        }
+
+        // MARK: - NavigationDelegate
 
         func webView(_ webView: WKWebView, shouldAllowDeprecatedTLSFor challenge: URLAuthenticationChallenge) async -> Bool {
             true
@@ -69,18 +84,41 @@ struct ArticleWebView: NSViewRepresentable {
             completionHandler(.performDefaultHandling, nil)
         }
 
-        func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
-            if navigationAction.navigationType == .linkActivated {
-                if let url = navigationAction.request.url {
-                    let components = URLComponents(url: url, resolvingAgainstBaseURL: false)
-                    if components?.scheme == "http" || components?.scheme == "https" {
+        func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, preferences: WKWebpagePreferences, decisionHandler: @escaping (WKNavigationActionPolicy, WKWebpagePreferences) -> Void) {
+            // handle (ignore) target="_blank" (open in new window) link as external
+            if navigationAction.targetFrame == nil, let externalURL = navigationAction.request.url, isValidatedLink(externalURL) {
+                NSWorkspace.shared.open(externalURL)
+                decisionHandler(.cancel, preferences)
+                return
+            } else {
+                if navigationAction.shouldPerformDownload {
+                    decisionHandler(.download, preferences)
+                } else {
+                    decisionHandler(.allow, preferences)
+                }
+            }
+            navigationType = navigationAction.navigationType
+        }
+
+        func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
+            if navigationResponse.canShowMIMEType, let url = navigationResponse.response.url, let mimeType = navigationResponse.response.mimeType {
+                if shouldHandleDownloadForMimeType(mimeType) {
+                    decisionHandler(.download)
+                } else {
+                    if navigationType == .linkActivated, isValidatedLink(url) {
                         NSWorkspace.shared.open(url)
                         decisionHandler(.cancel)
                         return
                     }
+                    decisionHandler(.allow)
                 }
+            } else {
+                decisionHandler(.download)
             }
-            decisionHandler(.allow)
+        }
+
+        func webView(_ webView: WKWebView, navigationResponse: WKNavigationResponse, didBecome download: WKDownload) {
+            debugPrint("new download: \(download), from: \(navigationResponse.response)")
         }
     }
 }
