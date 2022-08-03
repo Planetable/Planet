74153576f2d8792ab34725d4d5dc4356592b1374
Kai <kai@v2ex.com>
2022-08-03 20:01:57 +0800

    Handle basic operations from context menu in article web view.

--- Planet/Downloads/PlanetDownloadsWebView.swift
+++ Planet/Downloads/PlanetDownloadsWebView.swift
@@ -9,32 +9,146 @@ import Cocoa
 import WebKit
 
 
-class PlanetDownloadsWebView: WKWebView {
+/*
+ https://stackoverflow.com/questions/28801032/how-can-the-context-menu-in-wkwebview-on-the-mac-be-modified-or-overridden
+ */
+private class GlobalScriptMessageHandler: NSObject, WKScriptMessageHandler {
+    
+    public private(set) static var instance = GlobalScriptMessageHandler()
+    
+    public private(set) var nodeName: String?
+    public private(set) var nodeId: String?
+    public private(set) var hrefNodeName: String?
+    public private(set) var hrefNodeId: String?
+    public private(set) var href: String?
+    public private(set) var src: String?
+    
+    static private var WHOLE_PAGE_SCRIPT = """
+        window.oncontextmenu = (event) => {
+            var target = event.target
+            var src = target.src
+            var href = target.href
+            var parentElement = target
+            while (href == null && parentElement.parentElement != null) {
+                parentElement = parentElement.parentElement
+                href = parentElement.href
+            }
 
-    override func draw(_ dirtyRect: NSRect) {
-        super.draw(dirtyRect)
+            if (href == null) {
+                parentElement = null;
+            }
+
+            window.webkit.messageHandlers.oncontextmenu.postMessage({
+                nodeName: target.nodeName,
+                id: target.id,
+                hrefNodeName: parentElement?.nodeName,
+                hrefId: parentElement?.id,
+                href,
+                src
+            });
+        }
+        """
+    
+    private override init() {
+        super.init()
     }
+    
+    public func ensureHandles(configuration: WKWebViewConfiguration) {
+        var alreadyHandling = false
+        for userScript in configuration.userContentController.userScripts {
+            if userScript.source == GlobalScriptMessageHandler.WHOLE_PAGE_SCRIPT {
+                alreadyHandling = true
+            }
+        }
+        
+        if !alreadyHandling {
+            let userContentController = configuration.userContentController
+            userContentController.add(self, name: "oncontextmenu")
+            let userScript = WKUserScript(source: GlobalScriptMessageHandler.WHOLE_PAGE_SCRIPT, injectionTime: .atDocumentStart, forMainFrameOnly: false)
+            userContentController.addUserScript(userScript)
+        }
+    }
+    
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        if let body = message.body as? NSDictionary {
+            nodeName = body["nodeName"] as? String
+            nodeId = body["id"] as? String
+            hrefNodeName = body["hrefNodeName"] as? String
+            hrefNodeId = body["hrefId"] as? String
+            href = body["href"] as? String
+            src = body["src"] as? String
+        }
+    }
+}
 
+class PlanetDownloadsWebView: WKWebView {
+    
+    init() {
+        super.init(frame: CGRect(), configuration: WKWebViewConfiguration())
+        GlobalScriptMessageHandler.instance.ensureHandles(configuration: self.configuration)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func willOpenMenu(_ menu: NSMenu, with event: NSEvent) {
         for menuItem in menu.items {
             guard let identifier = menuItem.identifier else { continue }
             switch identifier {
             case Self.openLinkIdentifier:
-                debugPrint("open link")
+                menuItem.target = self
+                menuItem.action = #selector(openLinkAction(_:))
+                menuItem.isHidden = shouldHideSelectedMenuItem()
             case Self.openLinkInNewWindowIdentifier:
-                debugPrint("open link in new window")
+                menuItem.target = self
+                menuItem.action = #selector(openLinkAction(_:))
+                menuItem.isHidden = shouldHideSelectedMenuItem()
             case Self.downloadLinkedFileIdentifier:
-                debugPrint("download linked file")
+                menuItem.target = self
+                menuItem.action = #selector(downloadFileAction(_:))
+                menuItem.isHidden = shouldHideSelectedMenuItem()
             case Self.openImageInNewWindowIdentifier:
-                debugPrint("open image in new window")
+                menuItem.target = self
+                menuItem.action = #selector(openImageAction(_:))
+                menuItem.isHidden = shouldHideSelectedMenuItem()
             case Self.downloadImageIdentifier:
-                debugPrint("download image")
+                menuItem.target = self
+                menuItem.action = #selector(downloadFileAction(_:))
+                menuItem.isHidden = shouldHideSelectedMenuItem()
             default:
                 break
             }
         }
     }
+    
+    private func shouldHideSelectedMenuItem() -> Bool {
+        return GlobalScriptMessageHandler.instance.href == nil && GlobalScriptMessageHandler.instance.src == nil
+    }
+    
+    @objc private func openLinkAction(_ sender: NSMenuItem) {
+        if let urlString = GlobalScriptMessageHandler.instance.href {
+            let url = URL(string: urlString)!
+            NSWorkspace.shared.open(url)
+        }
+    }
 
+    @objc private func downloadFileAction(_ sender: NSMenuItem) {
+        if let _ = GlobalScriptMessageHandler.instance.href, let srcString = GlobalScriptMessageHandler.instance.src {
+            self.load(URLRequest(url: URL(string: srcString)!))
+        } else if let urlString = GlobalScriptMessageHandler.instance.href {
+            self.load(URLRequest(url: URL(string: urlString)!))
+        }
+    }
+    
+    @objc private func openImageAction(_ sender: NSMenuItem) {
+        if let _ = GlobalScriptMessageHandler.instance.href, let srcString = GlobalScriptMessageHandler.instance.src {
+            NSWorkspace.shared.open(URL(string: srcString)!)
+        } else if let urlString = GlobalScriptMessageHandler.instance.href {
+            NSWorkspace.shared.open(URL(string: urlString)!)
+        }
+    }
+    
 }
 
 
