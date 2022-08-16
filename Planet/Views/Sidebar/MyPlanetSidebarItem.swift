0d2f5fb14054857c6628bd8e17d2cc0dbde6a0de
Kai <kai@v2ex.com>
2022-08-16 22:08:59 +0800

    Add a divider in sidebar context menu.

--- Planet/Views/Sidebar/MyPlanetSidebarItem.swift
+++ Planet/Views/Sidebar/MyPlanetSidebarItem.swift
@@ -34,121 +34,127 @@ struct MyPlanetSidebarItem: View {
             LoadingIndicatorView()
                 .opacity(planet.isPublishing ? 1.0 : 0.0)
         }
-            .contextMenu {
-
-                    Button {
-                        do {
-                            try WriterStore.shared.newArticle(for: planet)
-                        } catch {
-                            PlanetStore.shared.alert(title: "Failed to launch writer")
-                        }
-                    } label: {
-                        Text("New Article")
+        .contextMenu {
+            Group {
+                Button {
+                    do {
+                        try WriterStore.shared.newArticle(for: planet)
+                    } catch {
+                        PlanetStore.shared.alert(title: "Failed to launch writer")
                     }
-
-                    Button {
-                        Task {
-                            try await planet.publish()
-                        }
-                    } label: {
-                        Text(planet.isPublishing ? "Publishing" : "Publish Planet")
-                    }
-                        .disabled(planet.isPublishing)
-
-                    Button {
-                        Task {
-                            try planet.copyTemplateAssets()
-                            try planet.articles.forEach { try $0.savePublic() }
-                            try planet.savePublic()
-                        }
-                    } label: {
-                        Text("Rebuild")
-                    }
-
-                    Divider()
-
-                    Button {
-                        NSPasteboard.general.clearContents()
-                        NSPasteboard.general.setString("planet://\(planet.ipns)", forType: .string)
-                    } label: {
-                        Text("Copy URL")
-                    }
-
-                    Button {
-                        NSPasteboard.general.clearContents()
-                        NSPasteboard.general.setString(planet.ipns, forType: .string)
-                    } label: {
-                        Text("Copy IPNS")
-                    }
-
-                    if hasWorldWideWeb() {
-                        Button {
-                            openWorldWideWeb(planet.publicBasePath)
-                        } label: {
-                            Text("Open in WorldWideWeb Server")
-                        }
-                    }
-                    
-                    Button {
-                        if let url = planet.browserURL {
-                            NSWorkspace.shared.open(url)
-                        }
-                    } label: {
-                        Text("Open in Public Gateway")
-                    }
-
-                    Button {
-                        isExportingPlanet = true
-                    } label: {
-                        Text("Export Planet")
-                    }
-
-                    Button {
-                        isShowingDeleteConfirmation = true
-                    } label: {
-                        Text("Delete Planet")
-                    }
-
-            }
-            .confirmationDialog(
-                Text("Are you sure you want to delete this planet? This action cannot be undone."),
-                isPresented: $isShowingDeleteConfirmation
-            ) {
-                Button(role: .destructive) {
-                    try? planet.delete()
-                    if case .myPlanet(let selectedPlanet) = planetStore.selectedView,
-                       planet == selectedPlanet {
-                        planetStore.selectedView = nil
-                    }
-                    PlanetStore.shared.myPlanets.removeAll { $0.id == planet.id }
                 } label: {
-                    Text("Delete")
+                    Text("New Article")
+                }
+
+                Button {
+                    Task {
+                        try await planet.publish()
+                    }
+                } label: {
+                    Text(planet.isPublishing ? "Publishing" : "Publish Planet")
+                }
+                .disabled(planet.isPublishing)
+
+                Button {
+                    Task {
+                        try planet.copyTemplateAssets()
+                        try planet.articles.forEach { try $0.savePublic() }
+                        try planet.savePublic()
+                    }
+                } label: {
+                    Text("Rebuild")
+                }
+
+                Divider()
+            }
+
+            Group {
+                Button {
+                    NSPasteboard.general.clearContents()
+                    NSPasteboard.general.setString("planet://\(planet.ipns)", forType: .string)
+                } label: {
+                    Text("Copy URL")
+                }
+
+                Button {
+                    NSPasteboard.general.clearContents()
+                    NSPasteboard.general.setString(planet.ipns, forType: .string)
+                } label: {
+                    Text("Copy IPNS")
+                }
+
+                if hasWorldWideWeb() {
+                    Button {
+                        openWorldWideWeb(planet.publicBasePath)
+                    } label: {
+                        Text("Open in WorldWideWeb Server")
+                    }
+                }
+
+                Button {
+                    if let url = planet.browserURL {
+                        NSWorkspace.shared.open(url)
+                    }
+                } label: {
+                    Text("Open in Public Gateway")
+                }
+
+                Divider()
+            }
+
+            Group {
+                Button {
+                    isExportingPlanet = true
+                } label: {
+                    Text("Export Planet")
+                }
+
+                Button {
+                    isShowingDeleteConfirmation = true
+                } label: {
+                    Text("Delete Planet")
                 }
             }
-            .fileImporter(
-                isPresented: $isExportingPlanet,
-                allowedContentTypes: [.directory]
-            ) { result in
-                if let url = try? result.get() {
-                    do {
-                        try planet.exportBackup(to: url)
-                        return
-                    } catch PlanetError.FileExistsError {
-                        PlanetStore.shared.alert(
-                            title: "Failed to Export Planet",
-                            message: """
+        }
+        .confirmationDialog(
+            Text("Are you sure you want to delete this planet? This action cannot be undone."),
+            isPresented: $isShowingDeleteConfirmation
+        ) {
+            Button(role: .destructive) {
+                try? planet.delete()
+                if case .myPlanet(let selectedPlanet) = planetStore.selectedView,
+                   planet == selectedPlanet {
+                    planetStore.selectedView = nil
+                }
+                PlanetStore.shared.myPlanets.removeAll { $0.id == planet.id }
+            } label: {
+                Text("Delete")
+            }
+        }
+        .fileImporter(
+            isPresented: $isExportingPlanet,
+            allowedContentTypes: [.directory]
+        ) { result in
+            if let url = try? result.get() {
+                do {
+                    try planet.exportBackup(to: url)
+                    return
+                } catch PlanetError.FileExistsError {
+                    PlanetStore.shared.alert(
+                        title: "Failed to Export Planet",
+                        message: """
                                      There is already an exported Planet in the destination. \
                                      We do not recommend override your backup. \
                                      Please choose another destination, or rename your previous backup.
                                      """
-                        )
-                        return
-                    } catch {
-                        // use general alert
-                    }
+                    )
+                    return
+                } catch {
+                    // use general alert
                 }
-                PlanetStore.shared.alert(title: "Failed to Export Planet", message: "Please try again.")
             }
+            PlanetStore.shared.alert(title: "Failed to Export Planet", message: "Please try again.")
+        }
     }
 
     private func hasWorldWideWeb() -> Bool {
