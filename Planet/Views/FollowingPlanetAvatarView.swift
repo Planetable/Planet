3c5e35cc3d99f921abbb3183546d2d0d97d77a3d
Livid <livid@v2ex.com>
2022-08-21 07:40:18 -0700

    Apply the same style for FollowingPlanetAvatarView

--- Planet/Views/FollowingPlanetAvatarView.swift
+++ Planet/Views/FollowingPlanetAvatarView.swift
@@ -12,18 +12,31 @@ struct FollowingPlanetAvatarView: View {
                     .aspectRatio(contentMode: .fit)
                     .frame(width: 80, height: 80, alignment: .center)
                     .cornerRadius(40)
-            } else {
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 40)
+                            .stroke(Color("BorderColor"), lineWidth: 1)
+                    )
+                    .shadow(color: Color.black.opacity(0.1), radius: 3, x: 0, y: 2)
+            }
+            else {
                 Text(planet.nameInitials)
                     .font(Font.custom("Arial Rounded MT Bold", size: 40))
                     .foregroundColor(Color.white)
                     .contentShape(Rectangle())
                     .frame(width: 80, height: 80, alignment: .center)
-                    .background(LinearGradient(
-                        gradient: ViewUtils.getPresetGradient(from: planet.id),
-                        startPoint: .top,
-                        endPoint: .bottom
-                    ))
+                    .background(
+                        LinearGradient(
+                            gradient: ViewUtils.getPresetGradient(from: planet.id),
+                            startPoint: .top,
+                            endPoint: .bottom
+                        )
+                    )
                     .cornerRadius(40)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 40)
+                            .stroke(Color("BorderColor"), lineWidth: 1)
+                    )
+                    .shadow(color: Color.black.opacity(0.1), radius: 3, x: 0, y: 2)
             }
         }
     }
