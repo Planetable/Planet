fcc3bcfbc1a59b050c674a2320149ccfe0e239f6
Livid <livid@v2ex.com>
2022-08-24 16:41:59 -0700

    Implement internal link detection for #65

--- Planet/Views/ArticleWebView.swift
+++ Planet/Views/ArticleWebView.swift
@@ -19,21 +19,35 @@ struct ArticleWebView: NSViewRepresentable {
         wv.navigationDelegate = context.coordinator
         wv.setValue(false, forKey: "drawsBackground")
         if url.isFileURL {
-            wv.loadFileURL(url, allowingReadAccessTo: url.deletingLastPathComponent().deletingLastPathComponent())
-        } else {
+            wv.loadFileURL(
+                url,
+                allowingReadAccessTo: url.deletingLastPathComponent().deletingLastPathComponent()
+            )
+        }
+        else {
             wv.load(URLRequest(url: url))
         }
 
-        NotificationCenter.default.addObserver(forName: .loadArticle, object: nil, queue: .main) { _ in
+        NotificationCenter.default.addObserver(forName: .loadArticle, object: nil, queue: .main) {
+            _ in
             Self.logger.log("Loading \(url), user agent: \(wv.customUserAgent ?? "")")
             if url.isFileURL {
-                wv.loadFileURL(url, allowingReadAccessTo: url.deletingLastPathComponent().deletingLastPathComponent())
-            } else {
+                wv.loadFileURL(
+                    url,
+                    allowingReadAccessTo: url.deletingLastPathComponent()
+                        .deletingLastPathComponent()
+                )
+            }
+            else {
                 wv.load(URLRequest(url: url))
             }
         }
 
-        NotificationCenter.default.addObserver(forName: .downloadArticleAttachment, object: nil, queue: nil) { n in
+        NotificationCenter.default.addObserver(
+            forName: .downloadArticleAttachment,
+            object: nil,
+            queue: nil
+        ) { n in
             Self.logger.log("Downloading \(url)")
             guard let url = n.object as? URL else { return }
             wv.load(URLRequest(url: url))
@@ -66,99 +80,182 @@ struct ArticleWebView: NSViewRepresentable {
             return false
         }
 
+        @MainActor private func findInternalArticleLink(url: URL) -> ArticleModel? {
+            let urlString = url.lastPathComponent
+            if let range = urlString.range(
+                of:
+                    #"[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}"#,
+                options: .regularExpression
+            ) {
+                let uuidString = urlString[range]
+                debugPrint("WKNavigationResponse: Found UUID: \(uuidString)")
+                if let article = PlanetStore.shared.selectedArticleList?.first(where: {
+                    $0.id.uuidString == uuidString
+                }) {
+                    if article.id.uuidString != PlanetStore.shared.selectedArticle?.id.uuidString {
+                        return article
+                    }
+                }
+            }
+            return nil
+        }
+
         // MARK: - NavigationDelegate
 
-        func webView(_ webView: WKWebView, shouldAllowDeprecatedTLSFor challenge: URLAuthenticationChallenge) async -> Bool {
+        func webView(
+            _ webView: WKWebView,
+            shouldAllowDeprecatedTLSFor challenge: URLAuthenticationChallenge
+        ) async -> Bool {
             true
         }
 
         func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         }
 
-        func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!)
+        {
         }
 
         func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
         }
 
-        func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+        func webView(
+            _ webView: WKWebView,
+            didFailProvisionalNavigation navigation: WKNavigation!,
+            withError error: Error
+        ) {
         }
 
-        func webView(_ webView: WKWebView, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+        func webView(
+            _ webView: WKWebView,
+            didReceive challenge: URLAuthenticationChallenge,
+            completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) ->
+                Void
+        ) {
             completionHandler(.performDefaultHandling, nil)
         }
 
-        func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, preferences: WKWebpagePreferences, decisionHandler: @escaping (WKNavigationActionPolicy, WKWebpagePreferences) -> Void) {
+        func webView(
+            _ webView: WKWebView,
+            decidePolicyFor navigationAction: WKNavigationAction,
+            preferences: WKWebpagePreferences,
+            decisionHandler: @escaping (WKNavigationActionPolicy, WKWebpagePreferences) -> Void
+        ) {
             // handle (ignore) target="_blank" (open in new window) link as external
-            if navigationAction.targetFrame == nil, let externalURL = navigationAction.request.url, isValidatedLink(externalURL) {
+            if navigationAction.targetFrame == nil, let externalURL = navigationAction.request.url,
+                isValidatedLink(externalURL)
+            {
                 NSWorkspace.shared.open(externalURL)
                 decisionHandler(.cancel, preferences)
                 return
-            } else {
+            }
+            else {
                 if navigationAction.shouldPerformDownload {
                     decisionHandler(.download, preferences)
-                } else {
+                }
+                else {
                     decisionHandler(.allow, preferences)
                 }
             }
             navigationType = navigationAction.navigationType
         }
 
-        @MainActor func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
-            if navigationResponse.canShowMIMEType, let url = navigationResponse.response.url, let mimeType = navigationResponse.response.mimeType {
+        @MainActor func webView(
+            _ webView: WKWebView,
+            decidePolicyFor navigationResponse: WKNavigationResponse,
+            decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void
+        ) {
+            if navigationResponse.canShowMIMEType, let url = navigationResponse.response.url,
+                let mimeType = navigationResponse.response.mimeType
+            {
                 if shouldHandleDownloadForMIMEType(mimeType) {
-                    debugPrint("WKNavigationResponse: .download branch 1 -> canShowMIMEType: \(navigationResponse.canShowMIMEType), url: \(String(describing: navigationResponse.response.url)), mimeType: \(String(describing: navigationResponse.response.mimeType))")
+                    debugPrint(
+                        "WKNavigationResponse: .download branch 1 -> canShowMIMEType: \(navigationResponse.canShowMIMEType), url: \(String(describing: navigationResponse.response.url)), mimeType: \(String(describing: navigationResponse.response.mimeType))"
+                    )
                     decisionHandler(.download)
-                } else {
+                }
+                else {
                     if navigationType == .linkActivated, isValidatedLink(url) {
-                        debugPrint("WKNavigationResponse: open in external browser -> canShowMIMEType: \(navigationResponse.canShowMIMEType), url: \(String(describing: navigationResponse.response.url)), mimeType: \(String(describing: navigationResponse.response.mimeType))")
+                        if let article = findInternalArticleLink(url: url) {
+                            Task { @MainActor in
+                                PlanetStore.shared.selectedArticle = article
+                            }
+                            decisionHandler(.cancel)
+                            return
+                        }
+                        debugPrint(
+                            "WKNavigationResponse: open in external browser -> canShowMIMEType: \(navigationResponse.canShowMIMEType), url: \(String(describing: navigationResponse.response.url)), mimeType: \(String(describing: navigationResponse.response.mimeType))"
+                        )
                         NSWorkspace.shared.open(url)
                         decisionHandler(.cancel)
                         return
                     }
-                    debugPrint("WKNavigationResponse: .allow -> canShowMIMEType: \(navigationResponse.canShowMIMEType), url: \(String(describing: navigationResponse.response.url)), mimeType: \(String(describing: navigationResponse.response.mimeType))")
+                    debugPrint(
+                        "WKNavigationResponse: .allow -> canShowMIMEType: \(navigationResponse.canShowMIMEType), url: \(String(describing: navigationResponse.response.url)), mimeType: \(String(describing: navigationResponse.response.mimeType))"
+                    )
                     decisionHandler(.allow)
                 }
-            } else {
-                debugPrint("WKNavigationResponse: .download branch 2 -> canShowMIMEType: \(navigationResponse.canShowMIMEType), url: \(String(describing: navigationResponse.response.url)), mimeType: \(String(describing: navigationResponse.response.mimeType))")
-                if let urlString = navigationResponse.response.url?.lastPathComponent, let range = urlString.range(of: #"[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}"#, options: .regularExpression) {
-                    let uuidString = urlString[range]
-                    debugPrint("WKNavigationResponse: Found UUID: \(uuidString)")
-                    if let article = PlanetStore.shared.selectedArticleList?.first(where: { $0.id.uuidString == uuidString }) {
-                        Task { @MainActor in
-                            PlanetStore.shared.selectedArticle = article
-                        }
-                        decisionHandler(.cancel)
-                        return
+            }
+            else {
+                debugPrint(
+                    "WKNavigationResponse: .download branch 2 -> canShowMIMEType: \(navigationResponse.canShowMIMEType), url: \(String(describing: navigationResponse.response.url)), mimeType: \(String(describing: navigationResponse.response.mimeType))"
+                )
+                if let url = navigationResponse.response.url,
+                    let article = findInternalArticleLink(url: url)
+                {
+                    Task { @MainActor in
+                        PlanetStore.shared.selectedArticle = article
                     }
+                    decisionHandler(.cancel)
+                    return
                 }
                 decisionHandler(.download)
             }
         }
 
-        func webView(_ webView: WKWebView, navigationResponse: WKNavigationResponse, didBecome download: WKDownload) {
+        func webView(
+            _ webView: WKWebView,
+            navigationResponse: WKNavigationResponse,
+            didBecome download: WKDownload
+        ) {
             // MARK: TODO: detect running downloads before start new one.
             download.delegate = self
         }
 
         // MARK: - DownloadDelegate
 
-        func download(_ download: WKDownload, decideDestinationUsing response: URLResponse, suggestedFilename: String, completionHandler: @escaping (URL?) -> Void) {
+        func download(
+            _ download: WKDownload,
+            decideDestinationUsing response: URLResponse,
+            suggestedFilename: String,
+            completionHandler: @escaping (URL?) -> Void
+        ) {
             let tempDir = URL(fileURLWithPath: NSTemporaryDirectory())
             let downloadsDir = tempDir.appendingPathComponent("Downloads")
             if !FileManager.default.fileExists(atPath: downloadsDir.path) {
-                try? FileManager.default.createDirectory(at: downloadsDir, withIntermediateDirectories: true)
+                try? FileManager.default.createDirectory(
+                    at: downloadsDir,
+                    withIntermediateDirectories: true
+                )
             }
             let downloadURL = downloadsDir.appendingPathComponent(suggestedFilename)
             if FileManager.default.fileExists(atPath: downloadURL.path) {
-                if let userDownloadsDir = FileManager.default.urls(for: .downloadsDirectory, in: .userDomainMask).first {
+                if let userDownloadsDir = FileManager.default.urls(
+                    for: .downloadsDirectory,
+                    in: .userDomainMask
+                ).first {
                     let downloadedURL = userDownloadsDir.appendingPathComponent(suggestedFilename)
                     try? FileManager.default.moveItem(at: downloadURL, to: downloadedURL)
                     NSWorkspace.shared.activateFileViewerSelecting([downloadedURL])
                 }
                 completionHandler(nil)
-            } else {
-                let downloadItem = PlanetDownloadItem(id: UUID(), created: Date(), download: download)
+            }
+            else {
+                let downloadItem = PlanetDownloadItem(
+                    id: UUID(),
+                    created: Date(),
+                    download: download
+                )
                 Task { @MainActor in
                     PlanetDownloadsViewModel.shared.addDownload(downloadItem)
                 }
@@ -172,18 +269,33 @@ struct ArticleWebView: NSViewRepresentable {
         }
 
         func downloadDidFinish(_ download: WKDownload) {
-            if let url = download.progress.fileURL, let userDownloadsDir = FileManager.default.urls(for: .downloadsDirectory, in: .userDomainMask).first {
+            if let url = download.progress.fileURL,
+                let userDownloadsDir = FileManager.default.urls(
+                    for: .downloadsDirectory,
+                    in: .userDomainMask
+                ).first
+            {
                 let downloadedURL = userDownloadsDir.appendingPathComponent(url.lastPathComponent)
                 try? FileManager.default.moveItem(at: url, to: downloadedURL)
                 NSWorkspace.shared.activateFileViewerSelecting([downloadedURL])
             }
         }
 
-        func download(_ download: WKDownload, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+        func download(
+            _ download: WKDownload,
+            didReceive challenge: URLAuthenticationChallenge,
+            completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) ->
+                Void
+        ) {
             completionHandler(.performDefaultHandling, nil)
         }
 
-        func download(_ download: WKDownload, willPerformHTTPRedirection response: HTTPURLResponse, newRequest request: URLRequest, decisionHandler: @escaping (WKDownload.RedirectPolicy) -> Void) {
+        func download(
+            _ download: WKDownload,
+            willPerformHTTPRedirection response: HTTPURLResponse,
+            newRequest request: URLRequest,
+            decisionHandler: @escaping (WKDownload.RedirectPolicy) -> Void
+        ) {
             decisionHandler(.allow)
         }
     }
