83d322a054915eb311ade469a16ef346ea91d5b6
Kai <kai@v2ex.com>
2022-11-07 00:09:44 +0800

    Added avatar view to move article menu item.

--- Planet/Views/My/MyArticleItemView.swift
+++ Planet/Views/My/MyArticleItemView.swift
@@ -152,9 +152,67 @@ struct MyArticleItemView: View {
                         }
                     }
                 } label: {
-                    Text("\(targetPlanet.name)")
+                    HStack (spacing: 4) {
+                        if let image = resizedAvatarImage(fromImage: targetPlanet.avatar) {
+                            Image(nsImage: image)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                        } else if let image = articleItemAvatarImage(withTitle: targetPlanet.nameInitials) {
+                            Image(nsImage: image)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                        }
+                        Text(targetPlanet.name)
+                        Spacer(minLength: 4)
+                    }
                 }
             }
         }
+    }
+
+    private func articleItemAvatarImage(withTitle title: String) -> NSImage? {
+        let size = CGSize(width: 24, height: 24)
+        if let font = NSFont(name: "Arial Rounded MT Bold", size: size.width / 2.0) {
+            let img = NSImage(size: size)
+            let t = NSAttributedString(string: title, attributes: [NSAttributedString.Key.font: font, NSAttributedString.Key.foregroundColor: NSColor.white])
+            let drawPoint = NSPoint(x: (size.width - t.size().width) / 2.0, y: (size.height - t.size().height) / 2.0)
+            let gradients = ViewUtils.presetGradients
+            let gradient = gradients.randomElement() ?? Gradient(colors: [.white, .gray])
+            img.lockFocus()
+            defer {
+                img.unlockFocus()
+            }
+            if let ctx = NSGraphicsContext.current {
+                ctx.imageInterpolation = .high
+                let targetRect = NSRect(origin: .zero, size: size)
+                let radius: CGFloat = size.width / 2.0
+                let path: NSBezierPath = NSBezierPath(roundedRect: targetRect, xRadius: radius, yRadius: radius)
+                path.addClip()
+                let gradient = NSGradient(starting: NSColor(gradient.stops.first?.color ?? .white), ending: NSColor(gradient.stops.last?.color ?? .gray))
+                gradient?.draw(in: targetRect, angle: -90)
+                t.draw(at: drawPoint)
+            }
+            return img
+        }
+        return nil
+    }
+
+    private func resizedAvatarImage(fromImage image: NSImage?) -> NSImage? {
+        guard let image = image else { return nil }
+        let size = NSSize(width: 24, height: 24)
+        let img = NSImage(size: size)
+        img.lockFocus()
+        defer {
+            img.unlockFocus()
+        }
+        if let ctx = NSGraphicsContext.current {
+            ctx.imageInterpolation = .high
+            let targetRect = NSRect(origin: .zero, size: size)
+            let radius: CGFloat = size.width / 2.0
+            let path: NSBezierPath = NSBezierPath(roundedRect: targetRect, xRadius: radius, yRadius: radius)
+            path.addClip()
+            image.draw(in: targetRect, from: NSRect(origin: .zero, size: image.size), operation: .copy, fraction: 1.0)
+        }
+        return img
     }
 }
