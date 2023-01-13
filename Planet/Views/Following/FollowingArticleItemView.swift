9e64d42b7166e0159975b602bbfdcb16081b6afa
Livid <livid@v2ex.com>
2023-01-12 17:47:16 -0800

    Remove dead links

--- Planet/Views/Following/FollowingArticleItemView.swift
+++ Planet/Views/Following/FollowingArticleItemView.swift
@@ -3,6 +3,8 @@ import SwiftUI
 struct FollowingArticleItemView: View {
     @ObservedObject var article: FollowingArticleModel
 
+    @State private var isShowingDeleteConfirmation = false
+
     var body: some View {
         HStack {
             VStack {
@@ -11,7 +13,8 @@ struct FollowingArticleItemView: View {
                         .renderingMode(.original)
                         .frame(width: 8, height: 8)
                         .padding(.all, 4)
-                } else {
+                }
+                else {
                     Circle()
                         .fill(Color.blue)
                         .frame(width: 8, height: 8)
@@ -32,74 +35,102 @@ struct FollowingArticleItemView: View {
                         if summary.count < 40 {
                             Spacer()
                         }
-                    } else if let content = article.content, content.count > 0 {
+                    }
+                    else if let content = article.content, content.count > 0 {
                         Text(content.prefix(280))
                             .foregroundColor(.secondary)
                         if content.count < 40 {
                             Spacer()
                         }
-                    } else {
+                    }
+                    else {
                         Spacer()
                     }
                 }
-                    .frame(height: 48)
+                .frame(height: 48)
                 HStack(spacing: 6) {
-                        Text(article.created.mmddyyyy())
+                    Text(article.created.mmddyyyy())
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                    if article.hasAudio {
+                        Text(Image(systemName: "headphones"))
                             .font(.caption)
                             .foregroundColor(.secondary)
-                        if article.hasAudio {
-                            Text(Image(systemName: "headphones"))
+                    }
+                    if article.hasVideo {
+                        Text(Image(systemName: "video"))
                             .font(.caption)
                             .foregroundColor(.secondary)
-                        }
-                        if article.hasVideo {
-                            Text(Image(systemName: "video"))
-                            .font(.caption)
-                            .foregroundColor(.secondary)
-                        }
+                    }
                     Spacer()
                 }
             }
         }
-            .contentShape(Rectangle())
-            .contextMenu {
-                VStack {
-                    Button {
-                        if article.read == nil {
-                            article.read = Date()
-                        } else {
-                            article.read = nil
-                        }
-                        try? article.save()
-                    } label: {
-                        Text(article.read == nil ? "Mark as Read" : "Mark as Unread")
+        .contentShape(Rectangle())
+        .contextMenu {
+            VStack {
+                Button {
+                    if article.read == nil {
+                        article.read = Date()
                     }
-                    Button {
-                        if article.starred == nil {
-                            article.starred = Date()
-                        } else {
-                            article.starred = nil
-                        }
-                        try? article.save()
-                    } label: {
-                        Text(article.starred == nil ? "Star" : "Unstar")
+                    else {
+                        article.read = nil
                     }
-                    Button {
-                        if let url = article.browserURL {
-                            NSPasteboard.general.clearContents()
-                            NSPasteboard.general.setString(url.absoluteString, forType: .string)
-                        }
-                    } label: {
-                        Text("Copy Public Link")
+                    try? article.save()
+                } label: {
+                    Text(article.read == nil ? "Mark as Read" : "Mark as Unread")
+                }
+                Button {
+                    isShowingDeleteConfirmation = true
+                } label: {
+                    Text("Delete Article")
+                }
+                Button {
+                    if article.starred == nil {
+                        article.starred = Date()
                     }
-                    Button {
-                        if let url = article.browserURL {
-                            NSWorkspace.shared.open(url)
-                        }
-                    } label: {
-                        Text("Open in Browser")
+                    else {
+                        article.starred = nil
                     }
+                    try? article.save()
+                } label: {
+                    Text(article.starred == nil ? "Star" : "Unstar")
+                }
+                Button {
+                    if let url = article.browserURL {
+                        NSPasteboard.general.clearContents()
+                        NSPasteboard.general.setString(url.absoluteString, forType: .string)
+                    }
+                } label: {
+                    Text("Copy Public Link")
+                }
+                Button {
+                    if let url = article.browserURL {
+                        NSWorkspace.shared.open(url)
+                    }
+                } label: {
+                    Text("Open in Browser")
                 }
             }
+        }
+        .confirmationDialog(
+            Text("Are you sure you want to delete this article? This action will remove it from your feed. However, if the article is still available on the source, it will reappear in your feed the next time you refresh it."),
+            isPresented: $isShowingDeleteConfirmation
+        ) {
+            Button(role: .destructive) {
+                if PlanetStore.shared.selectedArticle == article {
+                    PlanetStore.shared.selectedArticle = nil
+                }
+                article.delete()
+                if case .followingPlanet(let selectedPlanet) = PlanetStore.shared.selectedView {
+                    if let index = selectedPlanet.articles.firstIndex(of: article) {
+                        selectedPlanet.articles.remove(at: index)
+                    }
+                }
+                PlanetStore.shared.refreshSelectedArticles()
+            } label: {
+                Text("Delete")
+            }
+        }
     }
 }
