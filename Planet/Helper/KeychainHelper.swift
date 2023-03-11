39f7c7aebec6d5f9bc84e57f733e3ae0225eb247
Kai <kai@v2ex.com>
2023-03-11 18:40:24 +0800

    Use data protection in keychain.

--- Planet/Helper/KeychainHelper.swift
+++ Planet/Helper/KeychainHelper.swift
@@ -14,11 +14,12 @@ class KeychainHelper: NSObject {
     
     func saveData(_ data: Data, forKey key: String, withICloudSync sync: Bool = false) throws {
         let saveQuery: [String: Any] = [
-            kSecClass as String: kSecClassGenericPassword as String,
-            kSecAttrAccount as String: key,
-            kSecValueData as String: data,
-            kSecAttrSynchronizable as String: sync ? kCFBooleanTrue! : kCFBooleanFalse!
-        ]
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrAccount: key,
+            kSecValueData: data,
+            kSecUseDataProtectionKeychain: true,
+            kSecAttrSynchronizable: sync ? kCFBooleanTrue! : kCFBooleanFalse!
+        ] as [String: Any]
         Task(priority: .utility) {
             SecItemDelete(saveQuery as CFDictionary)
             let status = SecItemAdd(saveQuery as CFDictionary, nil)
@@ -30,12 +31,12 @@ class KeychainHelper: NSObject {
 
     func loadData(forKey key: String, withICloudSync sync: Bool = false) throws -> Data {
         let loadQuery: [String: Any] = [
-            kSecClass as String: kSecClassGenericPassword,
-            kSecAttrAccount as String: key,
-            kSecReturnData as String: kCFBooleanTrue!,
-            kSecMatchLimit as String: kSecMatchLimitOne,
-            kSecAttrSynchronizable as String: sync ? kCFBooleanTrue! : kCFBooleanFalse!
-        ]
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrAccount: key,
+            kSecReturnData: kCFBooleanTrue!,
+            kSecMatchLimit: kSecMatchLimitOne,
+            kSecAttrSynchronizable: sync ? kCFBooleanTrue! : kCFBooleanFalse!
+        ] as [String: Any]
         var item: CFTypeRef?
         let status = SecItemCopyMatching(loadQuery as CFDictionary, &item)
         guard status == errSecSuccess else {
@@ -52,11 +53,12 @@ class KeychainHelper: NSObject {
             throw PlanetError.KeyManagerSavingKeyError
         }
         let saveQuery: [String: Any] = [
-            kSecClass as String: kSecClassGenericPassword as String,
-            kSecAttrAccount as String: key,
-            kSecValueData as String: data,
-            kSecAttrSynchronizable as String: sync ? kCFBooleanTrue! : kCFBooleanFalse!
-        ]
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrAccount: key,
+            kSecValueData: data,
+            kSecUseDataProtectionKeychain: true,
+            kSecAttrSynchronizable: sync ? kCFBooleanTrue! : kCFBooleanFalse!
+        ] as [String: Any]
         Task(priority: .utility) {
             SecItemDelete(saveQuery as CFDictionary)
             let status = SecItemAdd(saveQuery as CFDictionary, nil)
@@ -68,12 +70,12 @@ class KeychainHelper: NSObject {
 
     func loadValue(forKey key: String, withICloudSync sync: Bool = false) throws -> String {
         let loadQuery: [String: Any] = [
-            kSecClass as String: kSecClassGenericPassword,
-            kSecAttrAccount as String: key,
-            kSecReturnData as String: kCFBooleanTrue!,
-            kSecMatchLimit as String: kSecMatchLimitOne,
-            kSecAttrSynchronizable as String: sync ? kCFBooleanTrue! : kCFBooleanFalse!
-        ]
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrAccount: key,
+            kSecReturnData: kCFBooleanTrue!,
+            kSecMatchLimit: kSecMatchLimitOne,
+            kSecAttrSynchronizable: sync ? kCFBooleanTrue! : kCFBooleanFalse!
+        ] as [String: Any]
         var item: CFTypeRef?
         let status = SecItemCopyMatching(loadQuery as CFDictionary, &item)
         guard status == errSecSuccess else {
@@ -90,21 +92,21 @@ class KeychainHelper: NSObject {
     
     func check(forKey key: String) -> Bool {
         let loadQuery: [String: Any] = [
-            kSecClass as String: kSecClassGenericPassword,
-            kSecAttrAccount as String: key,
-            kSecReturnData as String: kCFBooleanFalse!,
-            kSecMatchLimit as String: kSecMatchLimitOne
-        ]
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrAccount: key,
+            kSecReturnData: kCFBooleanFalse!,
+            kSecMatchLimit: kSecMatchLimitOne
+        ] as [String: Any]
         let status = SecItemCopyMatching(loadQuery as CFDictionary, nil)
         return status == errSecSuccess
     }
     
     func delete(forKey key: String, withICloudSync sync: Bool = false) throws {
         let deleteQuery: [String: Any] = [
-            kSecClass as String: kSecClassGenericPassword as String,
-            kSecAttrAccount as String: key,
-            kSecAttrSynchronizable as String: sync ? kCFBooleanTrue! : kCFBooleanFalse!
-        ]
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrAccount: key,
+            kSecAttrSynchronizable: sync ? kCFBooleanTrue! : kCFBooleanFalse!
+        ] as [String: Any]
         Task(priority: .utility) {
             let status = SecItemDelete(deleteQuery as CFDictionary)
             if status != errSecSuccess {
