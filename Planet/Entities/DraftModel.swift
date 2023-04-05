Planetable/Planet

commit 3c3e679002d29263723a1aa2741881d8a7a0d6e4

Reload resources after changing / resetting library location correctly.

Planet/Entities/MyPlanetModel.swift
@@ -70,63 +70,68 @@ class MyPlanetModel: Equatable, Hashable, Identifiable, ObservableObject, Codabl
     @Published var drafts: [DraftModel]! = nil
     @Published var articles: [MyArticleModel]! = nil
 
-    static let myPlanetsPath: URL = {
-        // ~/Library/Containers/xyz.planetable.Planet/Data/Documents/Planet/My/
+    static func myPlanetsPath() -> URL {
         let url = URLUtils.repoPath().appendingPathComponent("My", isDirectory: true)
         try! FileManager.default.createDirectory(at: url, withIntermediateDirectories: true)
         return url
-    }()
-    lazy var basePath = Self.myPlanetsPath.appendingPathComponent(id.uuidString, isDirectory: true)
-    lazy var infoPath = basePath.appendingPathComponent("planet.json", isDirectory: false)
-    lazy var articlesPath = basePath.appendingPathComponent("Articles", isDirectory: true)
-    lazy var avatarPath = basePath.appendingPathComponent("avatar.png", isDirectory: false)
-    lazy var faviconPath = basePath.appendingPathComponent("favicon.ico", isDirectory: false)
-    lazy var podcastCoverArtPath = basePath.appendingPathComponent("podcastCoverArt.png", isDirectory: false)
+    }
+    var basePath: URL {
+        return Self.myPlanetsPath().appendingPathComponent(self.id.uuidString, isDirectory: true)
+    }
+    var infoPath: URL {
+        return Self.myPlanetsPath().appendingPathComponent(self.id.uuidString, isDirectory: true).appendingPathComponent("planet.json", isDirectory: false)
+    }
+    var articlesPath: URL {
+        return Self.myPlanetsPath().appendingPathComponent(self.id.uuidString, isDirectory: true).appendingPathComponent("Articles", isDirectory: true)
+    }
+    var avatarPath: URL {
+        return Self.myPlanetsPath().appendingPathComponent(self.id.uuidString, isDirectory: true).appendingPathComponent("avatar.png", isDirectory: false)
+    }
+    var faviconPath: URL {
+        return Self.myPlanetsPath().appendingPathComponent(self.id.uuidString, isDirectory: true).appendingPathComponent("favicon.ico", isDirectory: false)
+    }
+    var podcastCoverArtPath: URL {
+        return Self.myPlanetsPath().appendingPathComponent(self.id.uuidString, isDirectory: true).appendingPathComponent("podcastCoverArt.png", isDirectory: false)
+    }
+    var draftsPath: URL {
+        return Self.myPlanetsPath().appendingPathComponent(self.id.uuidString, isDirectory: true).appendingPathComponent("Drafts", isDirectory: true)
+    }
+    var articleDraftsPath: URL {
+        return Self.myPlanetsPath().appendingPathComponent(self.id.uuidString, isDirectory: true).appendingPathComponent("Drafts", isDirectory: true)
+    }
 
-    lazy var draftsPath = basePath.appendingPathComponent("Drafts", isDirectory: true)
-    lazy var articleDraftsPath = articlesPath.appendingPathComponent("Drafts", isDirectory: true)
-
-    static let publicPlanetsPath: URL = {
-        // ~/Library/Containers/xyz.planetable.Planet/Data/Documents/Planet/Public/
+    static func publicPlanetsPath() -> URL {
         let url = URLUtils.repoPath().appendingPathComponent("Public", isDirectory: true)
         try! FileManager.default.createDirectory(at: url, withIntermediateDirectories: true)
         return url
-    }()
-    lazy var publicBasePath = Self.publicPlanetsPath.appendingPathComponent(
-        id.uuidString,
-        isDirectory: true
-    )
-    lazy var publicInfoPath = publicBasePath.appendingPathComponent(
-        "planet.json",
-        isDirectory: false
-    )
-    lazy var publicAvatarPath = publicBasePath.appendingPathComponent(
-        "avatar.png",
-        isDirectory: false
-    )
-    lazy var publicFaviconPath = publicBasePath.appendingPathComponent(
-        "favicon.ico",
-        isDirectory: false
-    )
-    lazy var publicPodcastCoverArtPath = publicBasePath.appendingPathComponent(
-        "podcastCoverArt.png",
-        isDirectory: false
-    )
-
-    lazy var publicIndexPath = publicBasePath.appendingPathComponent(
-        "index.html",
-        isDirectory: false
-    )
-    lazy var publicRSSPath = publicBasePath.appendingPathComponent(
-        "rss.xml",
-        isDirectory: false
-    )
-    lazy var publicPodcastPath = publicBasePath.appendingPathComponent(
-        "podcast.xml",
-        isDirectory: false
-    )
-
-    lazy var publicAssetsPath = publicBasePath.appendingPathComponent("assets", isDirectory: true)
+    }
+    var publicBasePath: URL {
+        return Self.publicPlanetsPath().appendingPathComponent(self.id.uuidString, isDirectory: true)
+    }
+    var publicInfoPath: URL {
+        return Self.publicPlanetsPath().appendingPathComponent(self.id.uuidString, isDirectory: true).appendingPathComponent("planet.json", isDirectory: false)
+    }
+    var publicAvatarPath: URL {
+        return Self.publicPlanetsPath().appendingPathComponent(self.id.uuidString, isDirectory: true).appendingPathComponent("avatar.png", isDirectory: false)
+    }
+    var publicFaviconPath: URL {
+        return Self.publicPlanetsPath().appendingPathComponent(self.id.uuidString, isDirectory: true).appendingPathComponent("favicon.ico", isDirectory: false)
+    }
+    var publicPodcastCoverArtPath: URL {
+        return Self.publicPlanetsPath().appendingPathComponent(self.id.uuidString, isDirectory: true).appendingPathComponent("podcastCoverArt.png", isDirectory: false)
+    }
+    var publicIndexPath: URL {
+        return Self.publicPlanetsPath().appendingPathComponent(self.id.uuidString, isDirectory: true).appendingPathComponent("index.html", isDirectory: false)
+    }
+    var publicRSSPath: URL {
+        return Self.publicPlanetsPath().appendingPathComponent(self.id.uuidString, isDirectory: true).appendingPathComponent("rss.xml", isDirectory: false)
+    }
+    var publicPodcastPath: URL {
+        return Self.publicPlanetsPath().appendingPathComponent(self.id.uuidString, isDirectory: true).appendingPathComponent("podcast.xml", isDirectory: false)
+    }
+    var publicAssetsPath: URL {
+        return Self.publicPlanetsPath().appendingPathComponent(self.id.uuidString, isDirectory: true).appendingPathComponent("assets", isDirectory: true)
+    }
 
     var template: Template? {
         TemplateStore.shared[templateName]

Planet/Entities/FollowingPlanetModel.swift
@@ -52,19 +52,23 @@ class FollowingPlanetModel: Equatable, Hashable, Identifiable, ObservableObject,
     @Published var telegramUsername: String?
     @Published var mastodonUsername: String?
 
-    static let followingPlanetsPath: URL = {
-        // ~/Library/Containers/xyz.planetable.Planet/Data/Documents/Planet/Following/
+    static func followingPlanetsPath() -> URL {
         let url = URLUtils.repoPath().appendingPathComponent("Following", isDirectory: true)
         try! FileManager.default.createDirectory(at: url, withIntermediateDirectories: true)
         return url
-    }()
-    lazy var basePath = Self.followingPlanetsPath.appendingPathComponent(
-        id.uuidString,
-        isDirectory: true
-    )
-    lazy var infoPath = basePath.appendingPathComponent("planet.json", isDirectory: false)
-    lazy var articlesPath = basePath.appendingPathComponent("Articles", isDirectory: true)
-    lazy var avatarPath = basePath.appendingPathComponent("avatar.png", isDirectory: false)
+    }
+    var basePath: URL {
+        return Self.followingPlanetsPath().appendingPathComponent(self.id.uuidString, isDirectory: true)
+    }
+    var infoPath: URL {
+        return Self.followingPlanetsPath().appendingPathComponent(self.id.uuidString, isDirectory: true).appendingPathComponent("planet.json", isDirectory: false)
+    }
+    var articlesPath: URL {
+        return Self.followingPlanetsPath().appendingPathComponent(self.id.uuidString, isDirectory: true).appendingPathComponent("Articles", isDirectory: true)
+    }
+    var avatarPath: URL {
+        return Self.followingPlanetsPath().appendingPathComponent(self.id.uuidString, isDirectory: true).appendingPathComponent("avatar.png", isDirectory: false)
+    }
 
     var nameInitials: String {
         let initials = name.components(separatedBy: .whitespaces).map { $0.prefix(1).capitalized }

Planet/Entities/PlanetStore.swift
@@ -27,7 +27,6 @@ enum PlanetDetailViewType: Hashable, Equatable {
 @MainActor class PlanetStore: ObservableObject {
     static let shared = PlanetStore()
     static let version = 1
-    static let repoVersionPath = URLUtils.repoPath().appendingPathComponent("Version")
 
     let logger = Logger(subsystem: Bundle.main.bundleIdentifier!, category: "PlanetStore")
 
@@ -170,7 +169,7 @@ enum PlanetDetailViewType: Hashable, Equatable {
     func load() throws {
         logger.info("Loading from planet repo")
         let myPlanetDirectories = try FileManager.default.contentsOfDirectory(
-            at: MyPlanetModel.myPlanetsPath,
+            at: MyPlanetModel.myPlanetsPath(),
             includingPropertiesForKeys: nil
         ).filter { $0.hasDirectoryPath }
         logger.info("Found \(myPlanetDirectories.count) my planets in repo")
@@ -181,7 +180,7 @@ enum PlanetDetailViewType: Hashable, Equatable {
         myPlanets = Array(myAllPlanets[myPlanetPartition...])
 
         let followingPlanetDirectories = try FileManager.default.contentsOfDirectory(
-            at: FollowingPlanetModel.followingPlanetsPath,
+            at: FollowingPlanetModel.followingPlanetsPath(),
             includingPropertiesForKeys: nil
         ).filter { $0.hasDirectoryPath }
         logger.info("Found \(followingPlanetDirectories.count) following planets in repo")
@@ -335,7 +334,7 @@ enum PlanetDetailViewType: Hashable, Equatable {
         try FileManager.default.copyItem(at: fromArticlePublicPath, to: targetArticlePublicPath)
 
         debugPrint("delete previous article")
-        try article.delete()
+        article.delete()
 
         let movedArticle = article
         movedArticle.planet = toPlanet

Planet/Helper/Saver.swift
@@ -18,12 +18,6 @@ class Saver: NSObject {
     static let coreDataPath: URL = applicationSupportDirectory.appendingPathComponent("Planet").appendingPathComponent("Planet.sqlite")
     static let documentDirectory: URL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
 
-    static let repoDirectory: URL = documentDirectory.appendingPathComponent("Planet", isDirectory: true)
-    static let publicDirectory: URL = MyPlanetModel.publicPlanetsPath
-
-    static let myPlanetsDirectory: URL = MyPlanetModel.myPlanetsPath
-    static let followingPlanetsDirectory: URL = FollowingPlanetModel.followingPlanetsPath
-
     private override init() {
         super.init()
     }
@@ -56,10 +50,10 @@ class Saver: NSObject {
 
     func prepareAllDirectories() {
         let directories: [URL] = [
-            Saver.repoDirectory,
-            Saver.publicDirectory,
-            Saver.myPlanetsDirectory,
-            Saver.followingPlanetsDirectory
+            URLUtils.repoPath(),
+            MyPlanetModel.publicPlanetsPath(),
+            MyPlanetModel.myPlanetsPath(),
+            FollowingPlanetModel.followingPlanetsPath()
         ]
 
         for directory in directories {
@@ -87,9 +81,9 @@ class Saver: NSObject {
             let fileName: String = "planet.json"
             var planetURL: URL
             if planet.isMyPlanet() {
-                planetURL = Saver.myPlanetsDirectory.appendingPathComponent(planetID.uuidString)
+                planetURL = MyPlanetModel.myPlanetsPath().appendingPathComponent(planetID.uuidString)
             } else {
-                planetURL = Saver.followingPlanetsDirectory.appendingPathComponent(planetID.uuidString)
+                planetURL = FollowingPlanetModel.followingPlanetsPath().appendingPathComponent(planetID.uuidString)
             }
             if FileManager.default.fileExists(atPath: planetURL.path) == false {
                 try? FileManager.default.createDirectory(at: planetURL, withIntermediateDirectories: true, attributes: nil)
@@ -177,7 +171,7 @@ class Saver: NSObject {
             }
 
             let legacyDirectory = Saver.applicationSupportDirectory.appendingPathComponent("planets").appendingPathComponent(planetID.uuidString)
-            let newDirectory = Saver.publicDirectory.appendingPathComponent(planetID.uuidString)
+            let newDirectory = MyPlanetModel.publicPlanetsPath().appendingPathComponent(planetID.uuidString)
 
             if FileManager.default.fileExists(atPath: legacyDirectory.path) {
                 try? FileManager.default.copyItem(at: legacyDirectory, to: newDirectory)

Planet/Helper/URLUtils.swift
@@ -50,25 +50,28 @@ struct URLUtils {
                             UserDefaults.standard.set(updatedBookmarkData, forKey: bookmarkKey)
                         }
                         if url.startAccessingSecurityScopedResource() {
-                            debugPrint("alternative library path is active: \(url.path)")
                             return planetURL
                         } else {
                             UserDefaults.standard.removeObject(forKey: .settingsLibraryLocation)
-                            debugPrint("failed to start accessing security scoped resource, abort.")
+                            debugPrint("failed to start accessing security scoped resource, abort & restore to default.")
                         }
                     }
                 } catch {
-                    debugPrint("failed to get planet library location: \(error), restore to default.")
                     UserDefaults.standard.removeObject(forKey: .settingsLibraryLocation)
+                    debugPrint("failed to get planet library location: \(error), restore to default.")
                 }
             } else {
                 UserDefaults.standard.removeObject(forKey: .settingsLibraryLocation)
             }
         }
+        return Self.defaultRepoPath
+    }
+    
+    static let defaultRepoPath: URL = {
         let url = Self.documentsPath.appendingPathComponent("Planet", isDirectory: true)
         try! FileManager.default.createDirectory(at: url, withIntermediateDirectories: true)
         return url
-    }
+    }()
 
     static let temporaryPath: URL = {
         let url = Self.cachesPath.appendingPathComponent("tmp", isDirectory: true)

Planet/Labs/Wallet/EthereumTransaction.swift
@@ -33,13 +33,6 @@ class EthereumTransaction: Codable, Identifiable {
     let memo: String
     let created: Date
 
-    static let walletsInfoPath: URL = {
-        // ~/Library/Containers/xyz.planetable.Planet/Data/Documents/Planet/Wallets/
-        let url = URLUtils.repoPath().appendingPathComponent("Wallets", isDirectory: true)
-        try! FileManager.default.createDirectory(at: url, withIntermediateDirectories: true)
-        return url
-    }()
-
     enum CodingKeys: String, CodingKey {
         case id
         case chainID
@@ -82,8 +75,14 @@ class EthereumTransaction: Codable, Identifiable {
         }
     }
 
+    static func walletsInfoPath() -> URL {
+        let url = URLUtils.repoPath().appendingPathComponent("Wallets", isDirectory: true)
+        try! FileManager.default.createDirectory(at: url, withIntermediateDirectories: true)
+        return url
+    }
+
     func save() throws {
-        let walletPath = EthereumTransaction.walletsInfoPath.appendingPathComponent(
+        let walletPath = Self.walletsInfoPath().appendingPathComponent(
             from,
             isDirectory: true
         )

Planet/Labs/Wallet/WalletAccountView.swift
@@ -190,7 +190,7 @@ struct WalletAccountView: View {
 
     private func loadTransactions() {
         debugPrint("Loading from the transactions folder")
-        let walletPath = EthereumTransaction.walletsInfoPath.appendingPathComponent(
+        let walletPath = EthereumTransaction.walletsInfoPath().appendingPathComponent(
             walletAddress,
             isDirectory: true
         )

Planet/Settings/PlanetSettingsGeneralView.swift
@@ -12,7 +12,24 @@ struct PlanetSettingsGeneralView: View {
 
     @EnvironmentObject private var viewModel: PlanetSettingsViewModel
     
-    @AppStorage(.settingsLibraryLocation) private var libraryLocation: String = UserDefaults.standard.string(forKey: .settingsLibraryLocation) ?? ""
+    @State private var libraryLocation: String = URLUtils.repoPath().path {
+        didSet {
+            Task(priority: .userInitiated) {
+                await MainActor.run {
+                    do {
+                        try PlanetStore.shared.load()
+                        try TemplateStore.shared.load()
+                        PlanetStore.shared.selectedArticle = nil
+                        PlanetStore.shared.selectedView = nil
+                        PlanetStore.shared.selectedArticleList = nil
+                        PlanetStore.shared.refreshSelectedArticles()
+                    } catch {
+                        debugPrint("failed to reload: \(error)")
+                    }
+                }
+            }
+        }
+    }
 
     @AppStorage(String.settingsPublicGatewayIndex) private var publicGatewayIndex: Int =
         UserDefaults.standard.integer(forKey: String.settingsPublicGatewayIndex)
@@ -58,7 +75,7 @@ struct PlanetSettingsGeneralView: View {
                         } label: {
                             Text("Reset")
                         }
-                        .disabled(URLUtils.repoPath().path == libraryLocation)
+                        .disabled(URLUtils.repoPath() == URLUtils.defaultRepoPath)
                         Spacer()
                     }
                     .padding(.top, -10)
@@ -146,8 +163,7 @@ struct PlanetSettingsGeneralView: View {
             try FileManager.default.copyItem(at: URLUtils.repoPath(), to: planetURL)
         }
         UserDefaults.standard.set(url.path, forKey: .settingsLibraryLocation)
-        try? TemplateStore.shared.load()
-        // MARK: TODO: restart planet.app
+        libraryLocation = URLUtils.repoPath().path
     }
 }
 

commit 3fac028e54e7d3615e704f57167651379912eaad

Fix drafts location bug introduced in https://github.com/Planetable/Planet/commit/3c3e679002d29263723a1aa2741881d8a7a0d6e4

Planet/Entities/DraftModel.swift
@@ -13,7 +13,7 @@ class DraftModel: Identifiable, Equatable, Hashable, Codable, ObservableObject {
 
     let logger = Logger(subsystem: Bundle.main.bundleIdentifier!, category: "Draft")
 
-    let id: UUID
+    var id: UUID
     @Published var date: Date
     @Published var title: String
     @Published var content: String
@@ -94,7 +94,11 @@ class DraftModel: Identifiable, Equatable, Hashable, Codable, ObservableObject {
     required init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         id = try container.decode(UUID.self, forKey: .id)
-        date = try container.decode(Date.self, forKey: .date)
+        if let date = try container.decodeIfPresent(Date.self, forKey: .date) {
+            self.date = date
+        } else {
+            self.date = Date()
+        }
         title = try container.decode(String.self, forKey: .title)
         content = try container.decode(String.self, forKey: .content)
         attachments = try container.decode([Attachment].self, forKey: .attachments)
@@ -122,9 +126,13 @@ class DraftModel: Identifiable, Equatable, Hashable, Codable, ObservableObject {
     }
 
     static func load(from directoryPath: URL, planet: MyPlanetModel) throws -> DraftModel {
+        let draftId = directoryPath.lastPathComponent
         let draftPath = directoryPath.appendingPathComponent("Draft.json", isDirectory: false)
         let data = try Data(contentsOf: draftPath)
         let draft = try JSONDecoder.shared.decode(DraftModel.self, from: data)
+        if draft.id != UUID(uuidString: draftId) {
+            draft.id = UUID(uuidString: draftId)!
+        }
         draft.target = .myPlanet(Unowned(planet))
         let contentSHA256String = draft.contentSHA256()
         debugPrint("Computed contentSHA256 for \(draft.title) during load/planet: \(contentSHA256String)")
@@ -374,7 +382,10 @@ class DraftModel: Identifiable, Equatable, Hashable, Codable, ObservableObject {
             let article = wrapper.value
             article.draft = nil
         }
-        if FileManager.default.fileExists(atPath: basePath.path) {
+        debugPrint("Deleting draft \(id) at \(basePath.path)")
+        // Remove the folder if it exists
+        var isDirectory: ObjCBool = false
+        if FileManager.default.fileExists(atPath: basePath.path, isDirectory: &isDirectory) {
             try FileManager.default.removeItem(at: basePath)
         }
     }

Planet/Entities/MyPlanetModel.swift
@@ -97,7 +97,7 @@ class MyPlanetModel: Equatable, Hashable, Identifiable, ObservableObject, Codabl
         return Self.myPlanetsPath().appendingPathComponent(self.id.uuidString, isDirectory: true).appendingPathComponent("Drafts", isDirectory: true)
     }
     var articleDraftsPath: URL {
-        return Self.myPlanetsPath().appendingPathComponent(self.id.uuidString, isDirectory: true).appendingPathComponent("Drafts", isDirectory: true)
+        return Self.myPlanetsPath().appendingPathComponent(self.id.uuidString, isDirectory: true).appendingPathComponent("Articles", isDirectory: true).appendingPathComponent("Drafts", isDirectory: true)
     }
 
     static func publicPlanetsPath() -> URL {
@@ -442,6 +442,7 @@ class MyPlanetModel: Equatable, Hashable, Identifiable, ObservableObject, Codabl
             at: planet.draftsPath,
             includingPropertiesForKeys: nil
         ).filter { $0.hasDirectoryPath }
+        debugPrint("Loading Planet \(planet.name) drafts from \(draftDirectories.count) directories")
         planet.drafts = draftDirectories.compactMap {
             try? DraftModel.load(from: $0, planet: planet)
         }

Planet/versioning.xcconfig
@@ -1,1 +1,1 @@
-CURRENT_PROJECT_VERSION = 1181
+CURRENT_PROJECT_VERSION = 1185