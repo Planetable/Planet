d1b843146307be91ad60d68a2eb4589f306035aa
Xin Liu <livid@v2ex.com>
2023-06-01 06:21:27 -0700

    Wait for 2.5 sec before load from local gateway

--- Planet/Entities/DraftModel.swift
+++ Planet/Entities/DraftModel.swift
@@ -360,11 +360,22 @@ class DraftModel: Identifiable, Equatable, Hashable, Codable, ObservableObject {
             PlanetStore.shared.selectedView = .myPlanet(planet)
             PlanetStore.shared.refreshSelectedArticles()
             // wrap it to delay the state change
-            Task { @MainActor in
-                if PlanetStore.shared.selectedArticle == article {
-                    NotificationCenter.default.post(name: .loadArticle, object: nil)
-                } else {
-                    PlanetStore.shared.selectedArticle = article
+            if planet.templateName == "Croptop" {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+                    // Croptop needs a delay here because it loads from the local gateway
+                    if PlanetStore.shared.selectedArticle == article {
+                        NotificationCenter.default.post(name: .loadArticle, object: nil)
+                    } else {
+                        PlanetStore.shared.selectedArticle = article
+                    }
+                }
+            } else {
+                Task { @MainActor in
+                    if PlanetStore.shared.selectedArticle == article {
+                        NotificationCenter.default.post(name: .loadArticle, object: nil)
+                    } else {
+                        PlanetStore.shared.selectedArticle = article
+                    }
                 }
             }
         }
