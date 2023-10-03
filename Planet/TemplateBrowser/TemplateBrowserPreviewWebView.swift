175bfc07509bae111de1fd45357c89dbd86a8af0
Xin Liu <livid@v2ex.com>
2023-10-02 21:49:58 -0700

    Fix template browser for Sonoma

--- Planet/TemplateBrowser/TemplateBrowserPreviewWebView.swift
+++ Planet/TemplateBrowser/TemplateBrowserPreviewWebView.swift
@@ -8,7 +8,6 @@
 import SwiftUI
 import WebKit
 
-
 struct TemplateBrowserPreviewWebView: NSViewRepresentable {
     public typealias NSViewType = WKWebView
 
@@ -23,20 +22,31 @@ struct TemplateBrowserPreviewWebView: NSViewRepresentable {
 
         wv.navigationDelegate = context.coordinator
         wv.setValue(false, forKey: "drawsBackground")
-        
+
         wv.customUserAgent = "Planet/" + PlanetUpdater.shared.appVersion()
 
         if url.isFileURL {
-            wv.loadFileURL(url, allowingReadAccessTo: url.deletingLastPathComponent())
-        } else {
+            wv.loadFileURL(
+                url,
+                allowingReadAccessTo: url.deletingLastPathComponent().deletingLastPathComponent()
+            )
+        }
+        else {
             wv.load(URLRequest(url: url))
         }
 
-        NotificationCenter.default.addObserver(forName: .loadTemplatePreview, object: nil, queue: .main) { _ in
-            debugPrint("Loading template preview from: \(url), user agent: \(wv.customUserAgent ?? "")")
+        NotificationCenter.default.addObserver(
+            forName: .loadTemplatePreview,
+            object: nil,
+            queue: .main
+        ) { _ in
+            debugPrint(
+                "Loading template preview from: \(url), user agent: \(wv.customUserAgent ?? "")"
+            )
             if url.isFileURL {
-                wv.loadFileURL(url, allowingReadAccessTo: url.deletingLastPathComponent())
-            } else {
+                wv.loadFileURL(url, allowingReadAccessTo: url.deletingLastPathComponent().deletingLastPathComponent())
+            }
+            else {
                 wv.load(URLRequest(url: url))
             }
         }
@@ -53,27 +63,44 @@ struct TemplateBrowserPreviewWebView: NSViewRepresentable {
             self.parent = parent
         }
 
-        func webView(_ webView: WKWebView, shouldAllowDeprecatedTLSFor challenge: URLAuthenticationChallenge) async -> Bool {
+        func webView(
+            _ webView: WKWebView,
+            shouldAllowDeprecatedTLSFor challenge: URLAuthenticationChallenge
+        ) async -> Bool {
             return true
         }
 
         func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         }
 
-        func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!)
+        {
         }
 
         func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
         }
 
-        func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+        func webView(
+            _ webView: WKWebView,
+            didFailProvisionalNavigation navigation: WKNavigation!,
+            withError error: Error
+        ) {
         }
 
-        func webView(_ webView: WKWebView, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+        func webView(
+            _ webView: WKWebView,
+            didReceive challenge: URLAuthenticationChallenge,
+            completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) ->
+                Void
+        ) {
             completionHandler(.performDefaultHandling, nil)
         }
 
-        func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        func webView(
+            _ webView: WKWebView,
+            decidePolicyFor navigationAction: WKNavigationAction,
+            decisionHandler: @escaping (WKNavigationActionPolicy) -> Void
+        ) {
             if navigationAction.navigationType == .linkActivated {
                 if let url = navigationAction.request.url {
                     let components = URLComponents(url: url, resolvingAgainstBaseURL: false)
@@ -90,7 +117,9 @@ struct TemplateBrowserPreviewWebView: NSViewRepresentable {
 }
 
 extension Notification.Name {
-    static let refreshTemplatePreview = Notification.Name("TemplateBrowserRefreshPreviewNotification")
+    static let refreshTemplatePreview = Notification.Name(
+        "TemplateBrowserRefreshPreviewNotification"
+    )
 
     static let loadTemplatePreview = Notification.Name("TemplateBrowserLoadPreviewNotification")
 }
