6b6f773ea048c3299546dcbce298cef8b803d49c
Livid <livid@v2ex.com>
2023-11-27 16:24:44 -0800

    Send emoji results to multiple relays

---
+++ tools/monitor-ipfs-peers/config.example.py
@@ -0,0 +1,5 @@
+# Put your nsec privatey key here
+nostr_private_key = "nsec..."
+
+# Relays to use
+nostr_relay_urls = ["wss://nostr.island.network", "wss://relay.damus.io"]
--- tools/monitor-ipfs-peers/monitor.py
+++ tools/monitor-ipfs-peers/monitor.py
@@ -1,4 +1,6 @@
 import arrow
+from datetime import datetime
+import pytz
 import socket
 import crayons
 import config
@@ -10,11 +12,14 @@ from nostr.message_type import ClientMessageType
 from nostr.key import PrivateKey
 
 
-def send_nostr_message(message_content, private_key_nsec, relay_url):
+def send_nostr_message(message_content, private_key_nsec):
+    print("Message: \n\n" + message_content)
+
     identity_pk = PrivateKey.from_nsec(private_key_nsec)
 
     relay_manager = RelayManager()
-    relay_manager.add_relay(relay_url)
+    for relay_url in config.nostr_relay_urls:
+        relay_manager.add_relay(relay_url)
     relay_manager.open_connections(
         {"cert_reqs": ssl.CERT_NONE}
     )  # NOTE: This disables ssl certificate verification
@@ -27,19 +32,28 @@ def send_nostr_message(message_content, private_key_nsec, relay_url):
     time.sleep(1)  # allow the messages to send
 
     relay_manager.close_connections()
+    print(crayons.green("Message sent to Nostr relays"))
 
 
 def check_connection(address, port):
     try:
         with socket.create_connection((address, port), timeout=10):
-            return f"Connection successful: {address} {port}"
+            return f"✅ Connection successful: {address} {port}"
     except socket.error as err:
-        return f"Error connecting to {address} {port} - {err}"
+        return f"☹️ Error connecting to {address} {port} - {err}"
+
+
+def format_timestamp(unix_timestamp):
+    # Convert the Unix timestamp to a datetime object
+    date = datetime.fromtimestamp(unix_timestamp, pytz.timezone("US/Pacific"))
+    # Format the date and time in the specified format
+    return date.strftime("%a - %b %d - %I:%M %p - %Z")
 
 
 def main():
-    filename = "peers.txt"  # Updated file name
-    o = str(arrow.get().to('US/Pacific')) + "\n\n"
+    filename = "peers.txt"
+    now = int(time.time())
+    o = format_timestamp(now) + "\n\n"
     with open(filename, "r") as file:
         for line in file:
             line = line.strip()
@@ -51,7 +65,7 @@ def main():
             address = parts[0]
             port = int(parts[1])
             o = o + check_connection(address, port) + "\n"
-        send_nostr_message(o, config.nostr_private_key, config.nostr_relay_url)
+        send_nostr_message(o, config.nostr_private_key)
 
 
 if __name__ == "__main__":
