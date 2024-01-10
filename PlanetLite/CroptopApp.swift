3b6ae93541b33f7373a409935828c634e84b2e55
Kai Luo <kai@v2ex.com>
2024-01-10 11:19:11 +0800

    WIP: swiftui lifecycle.

---
+++ PlanetLite/CroptopApp.swift
@@ -0,0 +1,16 @@
+//
+//  CroptopApp.swift
+//  Croptop
+//
+
+import SwiftUI
+
+
+@main
+struct CroptopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Hello Croptop")
+        }
+    }
+}
