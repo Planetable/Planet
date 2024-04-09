641e975f779cc1163f64f974b828c1e1a1dcedd8
Kai Luo <kai@v2ex.com>
2024-04-09 10:05:44 +0800

    Support quick share in planet article list.

--- Planet/Quick Share/PlanetQuickShareDropDelegate.swift
+++ Planet/Quick Share/PlanetQuickShareDropDelegate.swift
@@ -11,6 +11,38 @@ import UniformTypeIdentifiers
 class PlanetQuickShareDropDelegate: DropDelegate {
     init() {}
 
+    static func processDropInfo(_ info: DropInfo) async -> [URL] {
+        var urls: [URL] = []
+        if #available(macOS 13.0, *) {
+            for provider in info.itemProviders(for: [.image, .movie, .pdf]) {
+                if let url = try? await provider.loadItem(forTypeIdentifier: UTType.image.identifier) as? URL {
+                    urls.append(url)
+                    debugPrint("Drop file accepted: \(url)")
+                }
+                if let url = try? await provider.loadItem(forTypeIdentifier: UTType.pdf.identifier) as? URL {
+                    urls.append(url)
+                    debugPrint("Drop file accepted: \(url)")
+                }
+                if let url = try? await provider.loadItem(forTypeIdentifier: UTType.movie.identifier) as? URL {
+                    urls.append(url)
+                    debugPrint("Drop file accepted: \(url)")
+                }
+            }
+        } else {
+            var urls: [URL] = []
+            let supportedExtensions = ["png", "heic", "jpeg", "gif", "tiff", "jpg", "webp"]
+            for provider in info.itemProviders(for: [.fileURL]) {
+                if let item = try? await provider.loadItem(forTypeIdentifier: UTType.fileURL.identifier),
+                   let data = item as? Data,
+                   let path = URL(dataRepresentation: data, relativeTo: nil),
+                   supportedExtensions.contains(path.pathExtension) {
+                    urls.append(path)
+                }
+            }
+        }
+        return urls
+    }
+
     func dropUpdated(info: DropInfo) -> DropProposal? {
         return DropProposal(operation: .copy)
     }
@@ -22,56 +54,17 @@ class PlanetQuickShareDropDelegate: DropDelegate {
 
     func performDrop(info: DropInfo) -> Bool {
         Task { @MainActor in
-            if #available(macOS 13.0, *) {
-                var urls: [URL] = []
-                for provider in info.itemProviders(for: [.image, .movie, .pdf]) {
-                    if let url = try? await provider.loadItem(forTypeIdentifier: UTType.image.identifier) as? URL {
-                        urls.append(url)
-                        debugPrint("Drop file accepted: \(url)")
-                    }
-                    if let url = try? await provider.loadItem(forTypeIdentifier: UTType.pdf.identifier) as? URL {
-                        urls.append(url)
-                        debugPrint("Drop file accepted: \(url)")
-                    }
-                    if let url = try? await provider.loadItem(forTypeIdentifier: UTType.movie.identifier) as? URL {
-                        urls.append(url)
-                        debugPrint("Drop file accepted: \(url)")
-                    }
-                }
-                if urls.count > 0 {
-                    do {
-                        try PlanetQuickShareViewModel.shared.prepareFiles(urls)
-                    } catch {
-                        let alert = NSAlert()
-                        alert.messageText = "Failed to Add Attachments"
-                        alert.informativeText = error.localizedDescription
-                        alert.alertStyle = .warning
-                        alert.addButton(withTitle: "OK")
-                        alert.runModal()
-                    }
-                }
-            } else {
-                var urls: [URL] = []
-                let supportedExtensions = ["png", "heic", "jpeg", "gif", "tiff", "jpg", "webp"]
-                for provider in info.itemProviders(for: [.fileURL]) {
-                    if let item = try? await provider.loadItem(forTypeIdentifier: UTType.fileURL.identifier),
-                       let data = item as? Data,
-                       let path = URL(dataRepresentation: data, relativeTo: nil),
-                       supportedExtensions.contains(path.pathExtension) {
-                        urls.append(path)
-                    }
-                }
-                if urls.count > 0 {
-                    do {
-                        try PlanetQuickShareViewModel.shared.prepareFiles(urls)
-                    } catch {
-                        let alert = NSAlert()
-                        alert.messageText = "Failed to Add Attachments"
-                        alert.informativeText = error.localizedDescription
-                        alert.alertStyle = .warning
-                        alert.addButton(withTitle: "OK")
-                        alert.runModal()
-                    }
+            let urls: [URL] = await Self.processDropInfo(info)
+            if urls.count > 0 {
+                do {
+                    try PlanetQuickShareViewModel.shared.prepareFiles(urls)
+                } catch {
+                    let alert = NSAlert()
+                    alert.messageText = "Failed to Add Attachments"
+                    alert.informativeText = error.localizedDescription
+                    alert.alertStyle = .warning
+                    alert.addButton(withTitle: "OK")
+                    alert.runModal()
                 }
             }
         }
