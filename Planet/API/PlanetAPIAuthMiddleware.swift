9c67740f57298a5d9d59cdba75a083c49df84a05
Livid <livid@v2ex.com>
2024-08-27 22:33:58 -0700

    Use WWW-Authenticate for unauthorized requests

--- Planet/API/PlanetAPIAuthMiddleware.swift
+++ Planet/API/PlanetAPIAuthMiddleware.swift
@@ -1,24 +1,31 @@
-//
-//  PlanetAPIAuthMiddleware.swift
-//  Planet
-//
-
 import Foundation
 import Vapor
-
 
 struct PlanetAPIAuthMiddleware: AsyncMiddleware {
     let username: String
     let password: String
-    
+    let realm: String = "Planet API Server"
+
     func respond(to request: Request, chainingTo next: AsyncResponder) async throws -> Response {
         guard let authorization = request.headers.basicAuthorization else {
-            throw Abort(.unauthorized, reason: "Missing or invalid Authorization header")
+            return Response(
+                status: .unauthorized,
+                headers: [
+                    "WWW-Authenticate": "Basic realm=\"\(realm)\""
+                ]
+            )
         }
+
         if authorization.username == username && authorization.password == password {
             return try await next.respond(to: request)
-        } else {
-            throw Abort(.unauthorized, reason: "Invalid credentials")
+        }
+        else {
+            return Response(
+                status: .unauthorized,
+                headers: [
+                    "WWW-Authenticate": "Basic realm=\"\(realm)\""
+                ]
+            )
         }
     }
 }
