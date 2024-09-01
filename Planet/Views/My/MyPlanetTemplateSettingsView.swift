547881926903a89b59d7b7744e03ae22640ffbad
Livid <livid@v2ex.com>
2024-09-01 05:55:27 -0700

    Many improvements for Template Settings: JSON Import/Export, Color Preview

--- Planet/Views/My/MyPlanetTemplateSettingsView.swift
+++ Planet/Views/My/MyPlanetTemplateSettingsView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct MyPlanetTemplateSettingsView: View {
-    let CONTROL_CAPTION_WIDTH: CGFloat = 130
+    let CONTROL_CAPTION_WIDTH: CGFloat = 140
     let CONTROL_ROW_SPACING: CGFloat = 8
 
     @Environment(\.dismiss) var dismiss
@@ -18,6 +18,7 @@ struct MyPlanetTemplateSettingsView: View {
 
     @State private var currentSettings: [String: String] = [:]
     @State private var userSettings: [String: String] = [:]
+    @State private var colors: [String: Color] = [:]
 
     init(planet: MyPlanetModel) {
         self.planet = planet
@@ -32,51 +33,121 @@ struct MyPlanetTemplateSettingsView: View {
                 }
 
                 TabView {
-                    VStack(spacing: CONTROL_ROW_SPACING) {
-                        if let template = planet.template, let settings = template.settings,
-                            let keys = Array(settings.keys) as? [String]
-                        {
-                            ForEach(keys.sorted(), id: \.self) { key in
-                                HStack {
-                                    HStack {
-                                        Text("\(settings[key]?.name ?? "Name")")
-                                        Spacer()
-                                    }
-                                    .frame(width: CONTROL_CAPTION_WIDTH)
+                    ScrollView {
+                        VStack(spacing: CONTROL_ROW_SPACING) {
+                            if let template = planet.template, let settings = template.settings,
+                                let keys = Array(settings.keys) as? [String]
+                            {
 
-                                    TextField("", text: binding(key: key))
-                                        .textFieldStyle(.roundedBorder)
-                                }
-
-                                if let description = settings[key]?.description,
-                                    description.count > 0
-                                {
+                                ForEach(keys.sorted(), id: \.self) { key in
                                     HStack {
                                         HStack {
+                                            Text("\(settings[key]?.name ?? "Name")")
                                             Spacer()
                                         }
-                                        .frame(width: CONTROL_CAPTION_WIDTH + 10)
+                                        .frame(width: CONTROL_CAPTION_WIDTH)
 
-                                        Text(
-                                            description
-                                        )
-                                        .font(.footnote)
-                                        .foregroundColor(.secondary)
-                                        .fixedSize(horizontal: false, vertical: true)
+                                        TextField("", text: binding(key: key))
+                                            .textFieldStyle(.roundedBorder)
 
-                                        Spacer()
+                                        if key.hasSuffix("Color") {
+                                            ColorPicker(
+                                                "",
+                                                selection: bindingColor(key: key),
+                                                supportsOpacity: false
+                                            )
+                                        }
+                                    }
+
+                                    if let description = settings[key]?.description,
+                                        description.count > 0
+                                    {
+                                        HStack {
+                                            HStack {
+                                                Spacer()
+                                            }
+                                            .frame(width: CONTROL_CAPTION_WIDTH + 10)
+
+                                            Text(
+                                                description
+                                            )
+                                            .font(.footnote)
+                                            .foregroundColor(.secondary)
+                                            .fixedSize(horizontal: false, vertical: true)
+
+                                            Spacer()
+                                        }
                                     }
                                 }
                             }
                         }
-                    }
-                    .padding(16)
-                    .tabItem {
-                        Text("Template Settings")
-                    }
+                        .padding(16)
+                    }.frame(minHeight: 340, idealHeight: 410)
+                        .tabItem {
+                            Text("Template Settings")
+                        }
                 }
 
                 HStack(spacing: 8) {
+                    Button {
+                        // Export current settings to a JSON file
+                        let now = Int(Date().timeIntervalSince1970)
+                        let fileName =
+                            "\(planet.template?.id ?? "planet")-template-settings-\(now).json"
+                        let savePanel = NSSavePanel()
+                        savePanel.allowedFileTypes = ["json"]
+                        savePanel.nameFieldStringValue = fileName.lowercased()
+                        savePanel.begin { response in
+                            if response == .OK, let url = savePanel.url {
+                                do {
+                                    let data = try JSONSerialization.data(
+                                        withJSONObject: userSettings,
+                                        options: [.prettyPrinted, .sortedKeys]
+                                    )
+                                    try data.write(to: url)
+                                }
+                                catch {
+                                    debugPrint("Failed to save template settings: \(error)")
+                                }
+                            }
+                        }
+                    } label: {
+                        Text("Export")
+                            .frame(width: 50)
+                    }
+
+                    Button {
+                        // Import settings from a JSON file
+                        let openPanel = NSOpenPanel()
+                        openPanel.allowedFileTypes = ["json"]
+                        openPanel.begin { response in
+                            if response == .OK, let url = openPanel.url {
+                                do {
+                                    let data = try Data(contentsOf: url)
+                                    let settings =
+                                        try JSONSerialization.jsonObject(with: data)
+                                        as? [String: String]
+                                    if let settings = settings {
+                                        for (key, value) in settings {
+                                            // If key exists in user settings, update it
+                                            if userSettings[key] != nil {
+                                                userSettings[key] = value
+                                                if key.hasSuffix("Color") {
+                                                    colors[key] = Color(hex: value)
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                                catch {
+                                    debugPrint("Failed to load template settings: \(error)")
+                                }
+                            }
+                        }
+                    } label: {
+                        Text("Import")
+                            .frame(width: 50)
+                    }
                     Spacer()
 
                     Button {
@@ -107,8 +178,21 @@ struct MyPlanetTemplateSettingsView: View {
         .frame(width: 520, alignment: .top)
         .onAppear {
             currentSettings = planet.templateSettings()
+            let defaultSettings =
+                planet.template?.settings?.reduce(into: [:]) { (result, setting) in
+                    result[setting.key] = setting.value.defaultValue
+                } ?? [:]
+            for (key, value) in defaultSettings {
+                // If not set, use the default value
+                if currentSettings[key] == nil {
+                    currentSettings[key] = value
+                }
+            }
             for (key, value) in currentSettings {
                 userSettings[key] = value
+                if key.hasSuffix("Color") {
+                    colors[key] = Color(hex: value)
+                }
             }
         }
     }
@@ -120,6 +204,21 @@ struct MyPlanetTemplateSettingsView: View {
             },
             set: {
                 userSettings[key] = $0
+                if key.hasSuffix("Color") {
+                    colors[key] = Color(hex: $0)
+                }
+            }
+        )
+    }
+
+    private func bindingColor(key: String) -> Binding<Color> {
+        return Binding<Color>(
+            get: {
+                return colors[key] ?? Color.white
+            },
+            set: {
+                userSettings[key] = $0.toHexValue()
+                colors[key] = $0
             }
         )
     }
