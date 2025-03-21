173c20f32e49e88c9c70f3437220ba12e4ce077f
Kai Luo <kai@v2ex.com>
2025-03-21 11:29:13 +0800

    Use sendable PlanetDockTile.

--- Planet/Labs/Icon Gallery/PlanetDockPlugIn.swift
+++ Planet/Labs/Icon Gallery/PlanetDockPlugIn.swift
@@ -6,27 +6,15 @@ class PlanetDockPlugIn: NSObject, NSDockTilePlugIn {
     var targetPackageName: String = UserDefaults.standard.string(forKey: "PlanetDockIconLastPackageName") ?? ""
     
     func setDockTile(_ dockTile: NSDockTile?) {
+        let bundle = Bundle(identifier: "xyz.planetable.Planet.PlanetDockPlugIn")
+        let theDockTile = PlanetDockTile(dockTile: dockTile)
         let notificationName = Notification.Name("xyz.planetable.Planet.PlanetDockIconSyncPackageName")
         DistributedNotificationCenter.default().addObserver(forName: notificationName, object: nil, queue: nil) { [weak self] n in
             guard let selectedPackageName = n.object as? String else { return }
             UserDefaults.standard.set(selectedPackageName, forKey: "PlanetDockIconLastPackageName")
             self?.targetPackageName = selectedPackageName
-            self?.updateDock(forDockTile: dockTile, packageName: selectedPackageName)
+            theDockTile.update(with: selectedPackageName, bundle: bundle)
         }
-        updateDock(forDockTile: dockTile, packageName: targetPackageName)
-    }
-    
-    private func updateDock(forDockTile dockTile: NSDockTile?, packageName: String = "") {
-        guard packageName != "" else {
-            dockTile?.contentView = nil
-            dockTile?.display()
-            return
-        }
-        let iconName = packageName
-        let bundle = Bundle(identifier: "xyz.planetable.Planet.PlanetDockPlugIn")
-        guard let targetImage = bundle?.image(forResource: iconName) else { return }
-        let targetImageView = NSImageView(image: targetImage)
-        dockTile?.contentView = targetImageView
-        dockTile?.display()
+        theDockTile.update(with: targetPackageName, bundle: bundle)
     }
 }
