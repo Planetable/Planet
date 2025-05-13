823e870492a6a65f91646ffc6d887eaaa0d442a6
Xin Liu <livid@v2ex.com>
2025-05-13 02:45:15 -0700

    Clean up

--- Planet/Labs/Icon Gallery/PlanetDockPlugIn.swift
+++ Planet/Labs/Icon Gallery/PlanetDockPlugIn.swift
@@ -4,21 +4,18 @@ import Cocoa
 
 class PlanetDockPlugIn: NSObject, NSDockTilePlugIn {
     var targetPackageName: String = UserDefaults.standard.string(forKey: "PlanetDockIconLastPackageName") ?? ""
-    
+
     func setDockTile(_ dockTile: NSDockTile?) {
-//        let bundle = Bundle(identifier: "xyz.planetable.Planet.PlanetDockPlugIn")
-//        let theDockTile = PlanetDockTile(dockTile: dockTile)
         let notificationName = Notification.Name("xyz.planetable.Planet.PlanetDockIconSyncPackageName")
         DistributedNotificationCenter.default().addObserver(forName: notificationName, object: nil, queue: nil) { [weak self] n in
             guard let selectedPackageName = n.object as? String else { return }
             UserDefaults.standard.set(selectedPackageName, forKey: "PlanetDockIconLastPackageName")
             self?.targetPackageName = selectedPackageName
             self?.updateDock(forDockTile: dockTile, packageName: selectedPackageName)
-//            theDockTile.update(withPackageName: selectedPackageName, andBundle: bundle)
         }
-//        theDockTile.update(withPackageName: targetPackageName, andBundle: bundle)
+        updateDock(forDockTile: dockTile, packageName: targetPackageName)
     }
-    
+
     private func updateDock(forDockTile dockTile: NSDockTile?, packageName: String = "") {
         guard packageName != "" else {
             dockTile?.contentView = nil
@@ -31,6 +28,5 @@ class PlanetDockPlugIn: NSObject, NSDockTilePlugIn {
         let targetImageView = NSImageView(image: targetImage)
         dockTile?.contentView = targetImageView
         dockTile?.display()
-//        theDockTile.update(with: targetPackageName, bundle: bundle)
     }
 }
