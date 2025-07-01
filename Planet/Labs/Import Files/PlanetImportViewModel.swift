1fc79ca1fcf1d6fdae551a5d3b81ecd17f9a2556
Kai Luo <kai@v2ex.com>
2025-07-02 00:04:38 +0800

    Add basic local resource validation for selected markdown files.

--- Planet/Labs/Import Files/PlanetImportViewModel.swift
+++ Planet/Labs/Import Files/PlanetImportViewModel.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import SwiftUI
+import Logging
 
 
 enum InlineResourceType {
@@ -17,7 +18,7 @@ enum InlineResourceType {
     case markdownLink
     case markdownImage
 
-    private func regexPatternForElement() -> String {
+    func regexPattern() -> String {
         switch self {
         case .image:
             return #"<img\s+[^>]*src="([^"]+)""#
@@ -26,7 +27,8 @@ enum InlineResourceType {
         case .video:
             return #"<video\s+[^>]*src="([^"]+)""#
         case .file:
-            return #"<a\s+[^>]*href="([^"]+\.(zip|rar|7z|tar|gz|bz2|pdf|docx?|xlsx?|pptx?|csv|txt|mp3|wav|mp4|mov|avi|mkv|flac|jpg|jpeg|png|gif|bmp|svg|webp|swift|c|cpp|h|py|js|json|xml|html?|css|exe|dmg|app|apk|msi))"[^>]*>"#
+            // MARK: TODO: add from UTType
+            return #"<a\s+[^>]*href="([^"]+\.(zip|rar|7z|tar|gz|bz2|pdf|docx?|xlsx?|pptx?|csv|sh|txt|mp3|wav|mp4|mov|avi|mkv|flac|jpg|jpeg|png|gif|bmp|svg|webp|swift|c|cpp|h|py|js|json|xml|html?|css|exe|dmg|app|apk|msi))"[^>]*>"#
         case .markdownLink:
             return #"\[([^\]]*)\]\(([^)]+)\)"#
         case .markdownImage:
@@ -36,16 +38,18 @@ enum InlineResourceType {
 
     // MARK: -
 
-    func extractSourcesFromHTMLContent(_ htmlContent: String) -> [String] {
-        let pattern = self.regexPatternForElement()
-        var sources: [String] = []
-        
+    func extractSourcesFromHTMLContent(_ htmlContent: String) -> [URL] {
+        let pattern = self.regexPattern()
+        var sources: [URL] = []
+
         if let regex = try? NSRegularExpression(pattern: pattern, options: .caseInsensitive) {
             let matches = regex.matches(in: htmlContent, options: [], range: NSRange(htmlContent.startIndex..., in: htmlContent))
             for match in matches {
                 let rangeIndex = (self == .markdownLink || self == .markdownImage) ? 2 : 1
                 if let range = Range(match.range(at: rangeIndex), in: htmlContent) {
-                    sources.append(String(htmlContent[range]))
+                    if let url = URL(string: String(htmlContent[range])) {
+                        sources.append(url)
+                    }
                 }
             }
         }
@@ -56,6 +60,8 @@ enum InlineResourceType {
 
 class PlanetImportViewModel: ObservableObject {
     static let shared = PlanetImportViewModel()
+
+    static let logger = Logger(label: "Import Markdown Files")
 
     @Published var showingPreview: Bool = false
     @Published var previewURL: URL?
@@ -71,35 +77,34 @@ class PlanetImportViewModel: ObservableObject {
         markdownURLs = urls
     }
 
-    func validateMarkdown(_ url: URL) async throws -> Bool {
+    func validateMarkdown(_ markdownURL: URL) async throws -> Bool {
         Task { @MainActor in
-            if !validating.contains(url) {
-                validating.append(url)
+            if !validating.contains(markdownURL) {
+                validating.append(markdownURL)
             }
         }
         defer {
             Task { @MainActor in
                 try? await Task.sleep(nanoseconds: 500_000_000)
-                validating = validating.filter({ $0 != url })
+                validating = validating.filter({ $0 != markdownURL })
             }
         }
+        let baseMarkdownURL = markdownURL.deletingLastPathComponent()
+        let localURLs = try getLocalURLs(fromMarkdown: markdownURL)
+        let unaccessibleLocalSources = localURLs.filter { url in
+            if isResourceAccessible(withBaseURL: baseMarkdownURL, url: url) {
+                Self.logger.info(.init(stringLiteral: "\(url.path) is accessible"))
+                return false
+            }
+            Self.logger.info(.init(stringLiteral: "\(url.path) is inaccessible"))
+            return true
+        }
 
-        let markdownContent = try String(contentsOf: url)
-        let markdownLinks = extractMarkdownLinksFromContent(markdownContent)
-        let markdownImages = extractMarkdownImagesFromContent(markdownContent)
-        let images = extractImageSourcesFromHTMLContent(markdownContent)
-        let videos = extractVideoSourcesFromHTMLContent(markdownContent)
-        let audios = extractAudioSourcesFromHTMLContent(markdownContent)
-        let files = extractFileSourcesFromHTMLContent(markdownContent)
+        // print unaccessible links
+        Self.logger.info(.init(stringLiteral: "Unaccessible Local Sources:"))
+        Self.logger.info(.init(stringLiteral: unaccessibleLocalSources.map({ $0.absoluteString }).joined(separator: ", ")))
 
-        debugPrint("markdown links: \(markdownLinks)")
-        debugPrint("markdown images: \(markdownImages)")
-        debugPrint("image sources: \(images)")
-        debugPrint("video sources: \(videos)")
-        debugPrint("audio sources: \(audios)")
-        debugPrint("file sources: \(files)")
-
-        return true
+        return unaccessibleLocalSources.isEmpty
     }
 
     func prepareToImport() async throws {
@@ -138,27 +143,91 @@ class PlanetImportViewModel: ObservableObject {
         return importURL
     }
 
-    private func extractMarkdownLinksFromContent(_ content: String) -> [String] {
+    private func isResourceLocalURL(_ url: URL) -> Bool {
+        // Proper file URLs
+        if url.isFileURL {
+            return true
+        }
+
+        // No scheme & no host  →  root or relative path
+        if url.scheme == nil, url.host == nil {
+            return true
+        }
+
+        // Windows drive-letter path parsed as scheme “C”, “D”
+        if let s = url.scheme, s.count == 1, s.first!.isLetter {
+            return true
+        }
+
+        // UNC share literally written as \\SERVER\Share\file.png
+        if url.absoluteString.hasPrefix("\\\\") {
+            return true
+        }
+
+        return false
+    }
+
+    private func isResourceAccessible(withBaseURL baseURL: URL, url: URL) -> Bool {
+        return FileManager.default.fileExists(atPath: baseURL.appendingPathComponent(url.path).path) || FileManager.default.fileExists(atPath: url.path)
+    }
+
+    private func getLocalURLs(fromMarkdown url: URL) throws -> [URL] {
+        let markdownFileName = url.lastPathComponent
+        let markdownBaseURL = url.deletingLastPathComponent()
+        Self.logger.info(.init(stringLiteral: "Validating markdown file: \(markdownFileName), base url: \(markdownBaseURL)"))
+
+        let markdownContent = try String(contentsOf: url)
+
+        let markdownLinks = extractMarkdownLinksFromContent(markdownContent).filter({ isResourceLocalURL($0) })
+        let markdownImages = extractMarkdownImagesFromContent(markdownContent).filter({ isResourceLocalURL($0) })
+        let images = extractImageSourcesFromHTMLContent(markdownContent).filter({ isResourceLocalURL($0) })
+        let videos = extractVideoSourcesFromHTMLContent(markdownContent).filter({ isResourceLocalURL($0) })
+        let audios = extractAudioSourcesFromHTMLContent(markdownContent).filter({ isResourceLocalURL($0) })
+        let files = extractFileSourcesFromHTMLContent(markdownContent).filter({ isResourceLocalURL($0) })
+
+        Self.logger.info(.init(stringLiteral: "Local Markdown Links:"))
+        Self.logger.info(.init(stringLiteral: markdownLinks.map({ $0.absoluteString }).joined(separator: ", ")))
+
+        Self.logger.info(.init(stringLiteral: "Local Markdown Images:"))
+        Self.logger.info(.init(stringLiteral: markdownImages.map({ $0.absoluteString }).joined(separator: ", ")))
+
+        Self.logger.info(.init(stringLiteral: "Local HTML Image Sources:"))
+        Self.logger.info(.init(stringLiteral: images.map({ $0.absoluteString }).joined(separator: ", ")))
+
+        Self.logger.info(.init(stringLiteral: "Local HTML Video Sources:"))
+        Self.logger.info(.init(stringLiteral: videos.map({ $0.absoluteString }).joined(separator: ", ")))
+
+        Self.logger.info(.init(stringLiteral: "Local HTML Audio Sources:"))
+        Self.logger.info(.init(stringLiteral: audios.map({ $0.absoluteString }).joined(separator: ", ")))
+
+        Self.logger.info(.init(stringLiteral: "Local HTML File Sources:"))
+        Self.logger.info(.init(stringLiteral: files.map({ $0.absoluteString }).joined(separator: ", ")))
+
+        let uniqueLocalSources = Set(markdownLinks + markdownImages + images + videos + audios + files)
+        return Array(uniqueLocalSources) 
+    }
+
+    private func extractMarkdownLinksFromContent(_ content: String) -> [URL] {
         return InlineResourceType.markdownLink.extractSourcesFromHTMLContent(content)
     }
     
-    private func extractMarkdownImagesFromContent(_ content: String) -> [String] {
+    private func extractMarkdownImagesFromContent(_ content: String) -> [URL] {
         return InlineResourceType.markdownImage.extractSourcesFromHTMLContent(content)
     }
 
-    private func extractImageSourcesFromHTMLContent(_ content: String) -> [String] {
+    private func extractImageSourcesFromHTMLContent(_ content: String) -> [URL] {
         return InlineResourceType.image.extractSourcesFromHTMLContent(content)
     }
 
-    private func extractVideoSourcesFromHTMLContent(_ content: String) -> [String] {
+    private func extractVideoSourcesFromHTMLContent(_ content: String) -> [URL] {
         return InlineResourceType.video.extractSourcesFromHTMLContent(content)
     }
 
-    private func extractAudioSourcesFromHTMLContent(_ content: String) -> [String] {
+    private func extractAudioSourcesFromHTMLContent(_ content: String) -> [URL] {
         return InlineResourceType.audio.extractSourcesFromHTMLContent(content)
     }
 
-    private func extractFileSourcesFromHTMLContent(_ content: String) -> [String] {
+    private func extractFileSourcesFromHTMLContent(_ content: String) -> [URL] {
         return InlineResourceType.file.extractSourcesFromHTMLContent(content)
     }
 }
