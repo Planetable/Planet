3422c7f936b867f69c37a5b5fb201e97a6bf26ea
Livid <livid@v2ex.com>
2026-05-04 03:51:15 -0700

    Fix empty unfollow confirmation dialog

--- Planet/Views/Sidebar/FollowingPlanetSidebarItem.swift
+++ Planet/Views/Sidebar/FollowingPlanetSidebarItem.swift
@@ -111,19 +111,20 @@ struct FollowingPlanetSidebarItem: View {
                 Button {
                     isShowingArchiveConfirmation = true
                 } label: {
-                    Text("Archive Planet")
+                    Text(verbatim: L10n("Archive Planet"))
                 }
 
                 Button {
                     isShowingUnfollowConfirmation = true
                 } label: {
-                    Text("Unfollow")
+                    Text(verbatim: L10n("Unfollow"))
                 }
             }
         }
         .confirmationDialog(
-            Text("Are you sure you want to archive this planet? Archived planets will not be auto updated. You can later unarchive it from settings."),
-            isPresented: $isShowingArchiveConfirmation
+            Text(verbatim: L10n("Archive")),
+            isPresented: $isShowingArchiveConfirmation,
+            titleVisibility: .visible
         ) {
             Button() {
                 planet.archive()
@@ -134,12 +135,15 @@ struct FollowingPlanetSidebarItem: View {
                 planetStore.followingPlanets.removeAll { $0.id == planet.id }
                 planetStore.followingArchivedPlanets.insert(planet, at: 0)
             } label: {
-                Text("Archive")
+                Text(verbatim: L10n("Archive"))
             }
+        } message: {
+            Text(verbatim: L10n("Are you sure you want to archive this planet? Archived planets will not be auto updated. You can later unarchive it from settings."))
         }
         .confirmationDialog(
-            Text("Are you sure you want to unfollow this planet?"),
-            isPresented: $isShowingUnfollowConfirmation
+            Text(verbatim: L10n("Unfollow")),
+            isPresented: $isShowingUnfollowConfirmation,
+            titleVisibility: .visible
         ) {
             Button(role: .destructive) {
                 planetStore.followingPlanets.removeAll { $0.id == planet.id }
@@ -156,8 +160,10 @@ struct FollowingPlanetSidebarItem: View {
                     planetStore.selectedView = nil
                 }
             } label: {
-                Text("Unfollow")
+                Text(verbatim: L10n("Unfollow"))
             }
+        } message: {
+            Text(verbatim: L10n("Are you sure you want to unfollow this planet?"))
         }
 
     }
